3ec1ae0c0196de8a56c80c4840cb3448520d47d5
alishafulton <af.ui.design@gmail.com>
2024-07-14 17:09:42 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './Navbar.css';
-import name from '../../assets/name.png';
+import './Navbar.css'
+import name from '../../assets/alisha fulton.png'
 
 export default function Navbar () {
     return (
       <div className='navbar'>
+        <img src={name} alt="alisha fulton" />
         <ul className="nav-menu">
             <li>Hello is anybody out there</li>
         </ul>
