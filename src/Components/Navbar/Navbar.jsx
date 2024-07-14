a0e255cbc533decac725565aa1ffaefe42311020
alishafulton <af.ui.design@gmail.com>
2024-07-14 17:25:14 -0500

    g

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/name.png';
 export default function Navbar () {
     return (
       <div className='navbar'>
-        <img src="{logo}" alt="" />
+        <img src={logo} alt="" />
         <ul className="nav-menu">
             <li>Hello is anybody out there</li>
         </ul>
