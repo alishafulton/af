a2636a8046ec5fd3b7068d4c827b0411bad4c74d
alishafulton <af.ui.design@gmail.com>
2024-07-15 13:11:20 -0500

    g

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,10 +6,11 @@ import { Link, Outlet } from "react-router-dom";
 export default function Navbar () {
     return (
       <div className='navbar'>
+        <img src="{logo}" alt="" className="logo"/>
 
-        <Link to="/af/">Home</Link>
+        <Link to="/af/"><h2>Home</h2></Link>
         {" | "}
-        <Link to="/af/portfolio">portfolio</Link>
+        <Link to="/af/portfolio"><h2>portfolio</h2></Link>
 
       <Outlet />
 
