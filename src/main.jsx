0e31b777e7873bb4e74cc1ba904211dc2fc67703
alishafulton <af.ui.design@gmail.com>
2024-07-15 18:03:45 -0500

    d

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from './App';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/Home.tsx";
 import { Portfolio } from "./pages/Portfolio.tsx"
-import Blurb from './Components/Blurb/Blurb.jsx';
+import AboutPage from './Components/AboutPage/AboutPage.jsx';
+
 
 const router = createBrowserRouter([
   {
@@ -22,7 +23,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/af/about",
-        element: <Blurb/>
+        element: <AboutPage/>
       },
     ],
   },
