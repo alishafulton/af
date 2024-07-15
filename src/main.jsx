74ca51d98e6757068edc1f7f9a3c633606b13082
alishafulton <af.ui.design@gmail.com>
2024-07-15 12:48:22 -0500

    g

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { Portfolio } from "./pages/Portfolio.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/af/",
+    element: <App />,
+    children: [
+      {
+        path: "/af/",
+        element: <Home />,
+      },
+      {
+        path: "/af/portfolio",
+        element: <Portfolio/>
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
