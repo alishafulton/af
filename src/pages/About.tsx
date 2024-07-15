ad0567d9629d742009aa72eeb2f7db23d17751da
alishafulton <af.ui.design@gmail.com>
2024-07-15 14:32:32 -0500

    f

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,12 @@
-export function About() {
-    return "About";
-  }
+import React from "react";
+import selfie from '../../src/assets/CurrentPhoto.png';
+
+export default function About () {
+    return (
+      <div className='about'>
+        <img src={ selfie } alt=""/>
+        </div>
+      
+    )
+  };
+  
