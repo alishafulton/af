374714ca1bb9763204c219ac7be288c6dc86980e
alishafulton <af.ui.design@gmail.com>
2024-07-15 12:56:10 -0500

    Update Navbar.jsx

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -6,12 +6,13 @@ import { Link, Outlet } from "react-router-dom";
 export default function Navbar () {
     return (
       <div className='navbar'>
-        <img src={logo} alt="" />
-        <ul className="nav-menu">
-            <li><h2><Link to="/af/">Home</Link>link></h2></li>
-            <li><h2><Link to="/af/portfolio">portfolio</h2></li>
-        </ul>
-        <div className="nav-connect"><h3>contact</h3></div>
+
+        <Link to="/af/">Home</Link>
+        {" | "}
+        <Link to="/af/portfolio">portfolio</Link>
+
+      <Outlet />
+
       </div>
     )
   };
