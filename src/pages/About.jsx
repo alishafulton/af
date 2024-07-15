56c00c6a6a049d74db6d9dcc63a3e053f9db45b4
alishafulton <af.ui.design@gmail.com>
2024-07-15 14:48:30 -0500

    Update and rename About.jsx to About.tsx

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import selfie from '../assets/CurrentPhoto.png';
-
-export default function About () {
-    return (
-        <img src={ selfie } alt=""/>
-    );
-  }
-  
