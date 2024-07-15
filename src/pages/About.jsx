a3963062461fe1092892f5e7621e11c5497f7dd0
alishafulton <af.ui.design@gmail.com>
2024-07-15 14:45:25 -0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,7 @@ import selfie from '../assets/CurrentPhoto.png';
 
 export default function About () {
     return (
-      <div className='about'>
         <img src={ selfie } alt=""/>
-        </div>
-      
     );
   }
   
