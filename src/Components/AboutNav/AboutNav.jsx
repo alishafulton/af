640dba1bf29636b39ff867cb4b95846aefb79f67
alishafulton <af.ui.design@gmail.com>
2024-07-15 22:04:44 -0500

    Update AboutNav.jsx

--- src/Components/AboutNav/AboutNav.jsx
+++ src/Components/AboutNav/AboutNav.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './AboutNav.css';
-import Blurb from "../Blurb/Blurb";
 
 
 const AboutNav = () => {
@@ -66,18 +65,4 @@ const Specialbuton = ({ buttons, setButtons, handleButtonsChange }) => {
 
 
 
-
-export default function navsection2 () {
-    return (
-      <div className='navsection'>
-        <select name = "selectedAbout">
-        <h5>
-          <option value="about me">about me</option><br/>
-          <option value="extracurriculars">extracurriculars</option><br/>
-          <option value="spotify">spotify</option>
-          </h5>
-          </select>
-        </div>
-    );
-  }
 
