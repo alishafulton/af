0fbbb96e55eef2ed2b0aac58353361269d608375
alishafulton <af.ui.design@gmail.com>
2024-07-16 14:16:48 -0500

    b

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import './AboutPage.css'
 import Blurb from "../Blurb/Blurb";
 import Extra from "../Extracurriculars/Extracurriculars";
+import Spotify from "../Spotify/Spotify";
 
 export default function AboutPage() {
   const [isOn, setIsOn] = useState(true);
   const [isOn2, setIsOn2] = useState(false);
+  const [isOn3, setIsOn3] = useState(false);
 
   function toggle() {
     setIsOn((isOn) => !isOn);
     setIsOn2((isOn2) => !isOn2);
+    setIsOn3((isOn3) => !isOn3);
   }
 
 
@@ -17,13 +20,14 @@ export default function AboutPage() {
     <div className="results">
         <div className="links">
             <ul>
-            <button onClick={toggle}><li>about me</li></button> <br/>
+            <button onClick={toggle}><li>bio</li></button> <br/>
             <button onClick={toggle}><li>extracurriculars</li></button><br />
-            <button><li>spotify</li></button>
+            <button onClick={toggle}><li>spotify</li></button>
             </ul>
             </div>
       {isOn && <Blurb />}
       {isOn2 && <Extra />}
+      {isOn3 && <Spotify />}
     </div>
   );
 }
---
+++ src/Components/Spotify/Spotify.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Spotify () {
+    return (
+      <div className='blurb'>
+<iframe src="https://open.spotify.com/playlist/1yw34p3ZFZyUg45TlZm7hY?si=1653183a41d544ab}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+</div>);
+}
