9608b196b726baf85a0100203bf6c8d137d6c255
alishafulton <af.ui.design@gmail.com>
2024-07-16 11:26:43 -0500

    f

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -7,20 +7,18 @@ export default function AboutPage() {
   const [isOn, setIsOn] = useState(true);
   const [isOn2, setIsOn2] = useState(false);
 
-  function toggle1() {
+  function toggle() {
     setIsOn((isOn) => !isOn);
-  }
-
-  function toggle2() {
     setIsOn2((isOn2) => !isOn2);
   }
+
 
   return (
     <div className="results">
         <div className="links">
             <ul>
-            <button onClick={toggle1}><li>about me</li></button> <br/>
-            <button onClick={toggle2}><li>extracurriculars</li></button><br />
+            <button onClick={toggle}><li>about me</li></button> <br/>
+            <button onClick={toggle}><li>extracurriculars</li></button><br />
             <button><li>spotify</li></button>
             </ul>
             </div>
