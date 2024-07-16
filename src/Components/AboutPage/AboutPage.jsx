1e23d5d43177b71c8addea600c5788f33be9a6c4
alishafulton <af.ui.design@gmail.com>
2024-07-16 17:41:25 -0500

    Update AboutPage.jsx

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -39,7 +39,7 @@ export default function AboutPage() {
 
     function BioBlurb() {
       if (bioVisible) {
-        return <div><Blurb/></div>;
+        return "we're gonna figure iit out";
       }
         return null;
       }
