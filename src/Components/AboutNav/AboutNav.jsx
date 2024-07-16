223530493e78505a617ac4e8c9508725e70e1e03
alishafulton <af.ui.design@gmail.com>
2024-07-16 09:58:54 -0500

    d

--- src/Components/AboutNav/AboutNav.jsx
+++ src/Components/AboutNav/AboutNav.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 
-export default function LightSwitch() {
-  function handleClick() {
-    let bodyStyle = document.body.style;
-    if (bodyStyle.backgroundColor === 'black') {
-      bodyStyle.backgroundColor = 'white';
-    } else {
-      bodyStyle.backgroundColor = 'black';
-    }
+export default function AboutNav() {
+  const [isOn, setIsOn] = useState(false);
+
+  function toggle1() {
+    setIsOn((isOn) => !isOn);
   }
 
+
   return (
-    <button class="button" onClick={handleClick}>
-      about me
-    </button>
+    <div className="Results">
+      {isOn && <Blurb/>}
+      <button onClick={toggle1}>about me</button>
+    </div>
   );
 }
