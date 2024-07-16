7ea1fbbb71cf4da7a7c940afbd2967c61ca09612
alishafulton <af.ui.design@gmail.com>
2024-07-15 20:13:11 -0500

    Update AboutNav.jsx

--- src/Components/AboutNav/AboutNav.jsx
+++ src/Components/AboutNav/AboutNav.jsx
@@ -5,9 +5,9 @@ import './AboutNav.css';
 export default function AboutNav () {
     return (
       <div className='navsection'>
-        <h5>about me </h5><br/>
-        <h5>extracurriculars</h5><br/>
-        <h5>spotify</h5><br/>
+        <h5>about me <br/>
+        extracurriculars<br/>
+        spotify</h5><br/>
         </div>
     );
   };
