25b9fd73ed5f750da9b3bd6e278e334682fcd889
alishafulton <af.ui.design@gmail.com>
2024-07-16 15:25:03 -0500

    jj

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -4,30 +4,27 @@ import Blurb from "../Blurb/Blurb";
 import Extra from "../Extracurriculars/Extracurriculars";
 import Spotify from "../Spotify/Spotify";
 
-export default function AboutPage() {
-  const [isOn, setIsOn] = useState(true);
-  const [isOn2, setIsOn2] = useState(false);
-  const [isOn3, setIsOn3] = useState(false);
 
-  function toggle() {
-    setIsOn((isOn) => !isOn);
-    setIsOn2((isOn2) => !isOn2);
-    setIsOn3((isOn3) => !isOn3);
+export default function AboutPage() {
+    const [didHanSoloShootFirst, setDidHanSoloShootFirst] = useState(true);
+    const paragraphHanShotFirst = { Blurb };
+    const paragraphHanShotSecond = { Extra };
+    
+    function toggle() {
+      if(didHanSoloShootFirst === true) {
+          setDidHanSoloShootFirst(false);
+      } else if (didHanSoloShootFirst === false) {
+          setDidHanSoloShootFirst(true);
+      }
+    }
+  
+    return (
+      <div className='container'>
+        <button onClick={toggle}>Toggle Shoot</button>  
+
+        <img src={didHanSoloShootFirst ? HanShotFirst : HanShotSecond} />
+        <p>{didHanSoloShootFirst ? paragraphHanShotFirst : paragraphHanShotSecond}</p>
+      </div>
+    );
   }
 
-
-  return (
-    <div className="results">
-        <div className="links">
-            <ul>
-            <button onClick={toggle}><li>bio</li></button> <br/>
-            <button onClick={toggle}><li>extracurriculars</li></button><br />
-            <button onClick={toggle}><li>spotify</li></button>
-            </ul>
-            </div>
-      {isOn && <Blurb />}
-      {isOn2 && <Extra />}
-      {isOn3 && <Spotify />}
-    </div>
-  );
-}
