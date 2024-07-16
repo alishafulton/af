98320b6270bd358b0f1115cb15b2dc08473e823a
alishafulton <af.ui.design@gmail.com>
2024-07-16 10:16:07 -0500

    x

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -3,7 +3,7 @@ import './AboutPage.css'
 import Blurb from "../Blurb/Blurb";
 
 export default function AboutPage() {
-  const [isOn, setIsOn] = useState(false);
+  const [isOn, setIsOn] = useState(true);
 
   function toggle1() {
     setIsOn((isOn) => !isOn);
