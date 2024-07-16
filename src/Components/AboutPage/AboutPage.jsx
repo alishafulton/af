a23eb55defba378d27baca1f25efec792e71d082
alishafulton <af.ui.design@gmail.com>
2024-07-16 18:40:35 -0500

    Update AboutPage.jsx

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -55,7 +55,7 @@ export default function AboutPage() {
 
         function SpotifyBlurb() {
             if (spotifyVisible) {
-              return "spotiiiifyyyyyyyyyy";
+              return <div><iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1yw34p3ZFZyUg45TlZm7hY?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>;
             }
               return null;
             }
