0267b54e67cd1abc03ddb91b7553ceb458764720
alishafulton <af.ui.design@gmail.com>
2024-07-18 17:24:07 -0500

    z

--- src/Components/Blurb/Blurb.jsx
+++ src/Components/Blurb/Blurb.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import './Blurb.css';
 import selfie from '../../assets/CurrentPhoto.png'
 import Intro from "../Intro/Intro";
-import AnotherIntro from "../AnotherIntro/AnotherIntro";
-import ThirdIntro from "../ThirdIntro/ThirdIntro";
+
 
 export default function Blurb () {
     return (
       <div className='blurb'>
         <img src={ selfie } alt="" className="selfie"/>
 
-        <div className="intro">< Intro /> < AnotherIntro />< ThirdIntro /></div> 
+        <div className="intro">< Intro /></div> 
         </div>
 
     );
