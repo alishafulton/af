e08fa2066ca0bd3b8a330fb61601af5a7f86e18e
alishafulton <af.ui.design@gmail.com>
2024-07-18 10:10:59 -0500

    c

--- src/Components/Blurb/Blurb.jsx
+++ src/Components/Blurb/Blurb.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import './Blurb.css';
 import selfie from '../../assets/CurrentPhoto.png'
+import Intro from "../Hi Im Alisha/Intro";
 
 export default function Blurb () {
     return (
       <div className='blurb'>
         <img src={ selfie } alt="" className="selfie"/>
 
-        <div className="bio-blurb">
-          <div className="first-sentence">Hi, I'm Alisha. </div>
-        <h1>Working over ten years in customer service has given me a keen ability 
+        <div className="bio-blurb"> < Intro />
+        <p>Working over ten years in customer service has given me a keen ability 
         to assess customer needs and collaborate with others to achieve goals. 
         Now, I've transitioned into UX/UI, where I can hone both my interpersonal 
         skills and my creative talent to help brands develop and nurture customer 
@@ -17,7 +17,7 @@ export default function Blurb () {
         confidence in on-the-spot problem solving, as well as high levels of emotional 
         intelligence that I hope to bring to any team. I am excited to find a UX/UI Design 
         role that will leverage my customer-centric experience, foster my eagerness to learn, 
-        and allow me to create designs that will improve everyday user experiences.</h1>
+        and allow me to create designs that will improve everyday user experiences.</p>
         </div></div>
 
     );
---
+++ src/Components/Hi Im Alisha/Intro.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Intro () {
+    return (
+    <div className="first-sentence">Hi, I'm Alisha.</div>
+    );
+};
