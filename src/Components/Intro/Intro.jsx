b8a1bb0de70f8a19276ce3eb5ad8b42327d8c08d
alishafulton <af.ui.design@gmail.com>
2024-07-18 17:02:39 -0500

    Update Intro.jsx

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -5,12 +5,12 @@ import './Intro.css';
 export default function Intro () {
     return (
     <div className="intro"><div className="first-sentence">Hi, I'm Alisha!</div> 
-    <p>From learning to read at age three, to getting my Girl Scout Bronze, 
+    From learning to read at age three, to getting my Girl Scout Bronze, 
     Silver and Gold Awards, I have always been an avid learner. In high school,
     I took two years of a Practicum Education and Training with four
     internships in educational settings. I continued my education with a Bachelors of
     Science in Educational Studies. I developed my interpersonal skills
     in four more internships, this time focused less on traditional childhood
-    education, and instead, in and around my local community.</p></div>
+    education, and instead, in and around my local community.</div>
     );
 };
