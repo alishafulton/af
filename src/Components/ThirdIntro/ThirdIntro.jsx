66423b7f5bca37e4012654bd8496e48c70df8dfc
alishafulton <af.ui.design@gmail.com>
2024-07-18 17:13:55 -0500

    x

--- src/Components/ThirdIntro/ThirdIntro.jsx
+++ src/Components/ThirdIntro/ThirdIntro.jsx
@@ -4,7 +4,7 @@ import '../Intro/Intro.css';
 
 export default function ThirdIntro () {
     return (
-    <><div className="first-sentence">Spending over </div>ten years in customer service has given me a keen ability 
+    <div className="text"><div className="first-sentence">Spending over </div>ten years in customer service has given me a keen ability 
     to assess customer needs and collaborate with others to achieve goals. 
     Now, I've transitioned into UX/UI, where I can hone both my interpersonal 
     skills and my creative talent to help brands develop and nurture customer 
@@ -12,6 +12,6 @@ export default function ThirdIntro () {
     confidence in on-the-spot problem solving, as well as high levels of emotional 
     intelligence that I hope to bring to any team. I am excited to find a UX/UI Design 
     role that will leverage my customer-centric experience, foster my eagerness to learn, 
-    and allow me to create designs that will improve everyday user experiences.</>
+    and allow me to create designs that will improve everyday user experiences.</div>
     );
 };
