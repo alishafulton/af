b08038e5c7bcdafd23cca9235d285f2ddd639c90
alishafulton <af.ui.design@gmail.com>
2024-07-18 10:03:35 -0500

    Update Blurb.jsx

--- src/Components/Blurb/Blurb.jsx
+++ src/Components/Blurb/Blurb.jsx
@@ -9,7 +9,7 @@ export default function Blurb () {
 
         <div className="bio-blurb">
           <div className="first-sentence">Hi, I'm Alisha. </div>
-        Working over ten years in customer service has given me a keen ability 
+        <h1>Working over ten years in customer service has given me a keen ability 
         to assess customer needs and collaborate with others to achieve goals. 
         Now, I've transitioned into UX/UI, where I can hone both my interpersonal 
         skills and my creative talent to help brands develop and nurture customer 
@@ -19,6 +19,6 @@ export default function Blurb () {
         role that will leverage my customer-centric experience, foster my eagerness to learn, 
         and allow me to create designs that will improve everyday user experiences.</h1>
         </div></div>
-        </div>
+
     );
   };
