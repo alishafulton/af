c5ff480ab6a58779b27a7c72ec39658549072ce3
alishafulton <af.ui.design@gmail.com>
2024-07-19 10:30:27 -0500

    l

--- src/Components/Extracurriculars/Extracurriculars.jsx
+++ src/Components/Extracurriculars/Extracurriculars.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import './Extracurriculars.css';
 import pic1 from '../../assets/IMG_0640.png'
+import pic2 from '../../assets/IMG_0549.png'
+import pic3 from '../../assets/IMG_0556.png'
+import pic4 from '../../assets/IMG_0565.png'
+import pic5 from '../../assets/IMG_0573.png'
+import pic6 from '../../assets/IMG_0576.png'
+import pic7 from '../../assets/IMG_0658.png'
+import pic8 from '../../assets/IMG_0723.png'
+import pic9 from '../../assets/IMG_0739.png'
+import pic10 from '../../assets/IMG_0940.png'
+import pic11 from '../../assets/IMG_0944.png'
+import pic12 from '../../assets/IMG_0965.png'
+import pic13 from '../../assets/IMG_0967.png'
+
 
 export default function Extra () {
     return (
       <div>
         <img src={ pic1 } alt="" className="pic"/>
+        <img src={ pic2 } alt="" className="pic"/>
+        <img src={ pic3 } alt="" className="pic"/>
+        <img src={ pic4 } alt="" className="pic"/>
+        <img src={ pic5 } alt="" className="pic"/>
+        <img src={ pic6 } alt="" className="pic"/>
+        <img src={ pic7 } alt="" className="pic"/>
+        <img src={ pic8 } alt="" className="pic"/>
+        <img src={ pic9 } alt="" className="pic"/>
+        <img src={ pic10 } alt="" className="pic"/>
+        <img src={ pic11 } alt="" className="pic"/>
+        <img src={ pic12 } alt="" className="pic"/>
+        <img src={ pic13 } alt="" className="pic"/>
+
         </div>
     );
   };
