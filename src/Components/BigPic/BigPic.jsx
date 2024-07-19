e936f155560b0301de4b43b11b8c506f9fdb0063
alishafulton <af.ui.design@gmail.com>
2024-07-19 15:08:54 -0500

    b

--- src/Components/BigPic/BigPic.jsx
+++ src/Components/BigPic/BigPic.jsx
@@ -3,6 +3,6 @@ import './BigPic.css';
 
 export default function BigPic () {
     return (
-        <div className="headliner"><img src={ before } alt="" className="bigpic"/><div className="headline-text">March 2024</div></div>
+        <div className="headliner"><div className="headline-text">March 2024</div><img src={ before } alt="" className="bigpic"/></div>
     );
 };
