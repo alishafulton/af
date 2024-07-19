074d41f5213124b0acad061e0c6b4bf33857fd92
alishafulton <af.ui.design@gmail.com>
2024-07-19 10:23:07 -0500

    z

--- src/Components/Extracurriculars/Extracurriculars.jsx
+++ src/Components/Extracurriculars/Extracurriculars.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Extracurriculars.css';
-import pic1 from '../../assets/IMG_0640.HEIC'
+import pic1 from '../../assets/IMG_0640.png'
 
 export default function Extra () {
     return (
