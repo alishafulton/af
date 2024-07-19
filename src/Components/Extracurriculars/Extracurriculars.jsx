4f46116b43658c20deadffc3b08825a01bf643db
alishafulton <af.ui.design@gmail.com>
2024-07-19 14:24:26 -0500

    y

--- src/Components/Extracurriculars/Extracurriculars.jsx
+++ src/Components/Extracurriculars/Extracurriculars.jsx
@@ -17,11 +17,12 @@ export default function Extra () {
     return (
       <div className="extras">
         <img src={ before } alt="" className="bigpic"/><br/>
-        In March 2024, my cousin and I set out to screen 
+        <div className="first-sentence">In March 2024,</div><div className="text">my cousin and I set out to screen 
         in their 200 square foot porch. It took us some time to
         put idea into action, but after a couple days of discussing
         what methods we would take while getting swarmed by crane 
         flies, we picked a day to get started.
+        <img src={ wood } alt="" className="smallpic-v"/>
         Brimming with excitement, we triple-measured and wrote down
         exactly what we needed to buy on our first trip to Home Depot.
         We borrowed an SUV to go back and forth to the store and bought
@@ -33,8 +34,8 @@ export default function Extra () {
         went perfectly. Believe it or not, I even hammered myself a black eye.
         But with help from my aunt and two other cousins, a lot of
         patient Home Depot employees, and our iPhone calculators,
-        we did it and we did it well. 
-        <img src={ wood } alt="" className="smallpic-v"/>
+        we did it and we did it well. </div>
+        
         <img src={ girls } alt="" className="pic"/>
         <img src={ pic1 } alt="" className="pic"/>
         <img src={ pic4 } alt="" className="pic"/>
