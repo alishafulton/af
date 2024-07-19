8108d8dacf1ac1ef4f2566891943e0901109f4bc
alishafulton <af.ui.design@gmail.com>
2024-07-19 10:45:44 -0500

    Update Extracurriculars.jsx

--- src/Components/Extracurriculars/Extracurriculars.jsx
+++ src/Components/Extracurriculars/Extracurriculars.jsx
@@ -7,11 +7,9 @@ import pic4 from '../../assets/IMG_0565.png'
 import pic5 from '../../assets/IMG_0573.png'
 import pic6 from '../../assets/IMG_0576.png'
 import pic7 from '../../assets/IMG_0658.png'
-import pic8 from '../../assets/IMG_0723.png'
 import pic9 from '../../assets/IMG_0739.png'
 import pic10 from '../../assets/IMG_0940.png'
 import pic11 from '../../assets/IMG_0944.png'
-import pic12 from '../../assets/IMG_0965.png'
 import pic13 from '../../assets/IMG_0967.png'
 
 
@@ -25,11 +23,9 @@ export default function Extra () {
         <img src={ pic5 } alt="" className="pic"/>
         <img src={ pic6 } alt="" className="pic"/>
         <img src={ pic7 } alt="" className="pic"/>
-        <img src={ pic8 } alt="" className="pic"/>
         <img src={ pic9 } alt="" className="pic"/>
         <img src={ pic10 } alt="" className="pic"/>
         <img src={ pic11 } alt="" className="pic"/>
-        <img src={ pic12 } alt="" className="pic"/>
         <img src={ pic13 } alt="" className="pic"/>
 
         </div>
