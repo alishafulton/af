608fc9e7da35f4fd20d3171f2000ea1f9151a002
alishafulton <af.ui.design@gmail.com>
2024-07-20 10:55:03 -0500

    t

--- src/Components/Sprig/SprigPrototype.tsx
+++ src/Components/Sprig/SprigPrototype.tsx
@@ -23,20 +23,17 @@ export const SprigPrototype = ({
 
   }: SprigProps) => {
     const url = new URL(link);
+    url.pathname = url.pathname.replace(/\/intl-\w+\//, "/");
 
 
     return (
     
     <iframe 
-    src={'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F4Sc1LEgwnnOIf8Z31dkYtD%2FSPRIG%3Fpage-id%3D34%253A2%26node-id%3D230-347%26viewport%3D369%252C25%252C0.26%26t%3DHD5UMDuOwPQIH59t-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D230%253A345'}
+    src={`https://www.figma.com/embed${url.pathname}`}
     width={width}
     height={height}
     frameBorder={frameBorder}
     allow={allow}
-    style={{
-      borderRadius: 8,
-      ...style
-    }}
   />
     
     
