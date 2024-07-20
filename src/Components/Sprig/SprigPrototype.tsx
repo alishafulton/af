52bb1122a2440e33aa06855d0d21aeee2bd0b964
alishafulton <af.ui.design@gmail.com>
2024-07-20 10:06:16 -0500

    e

---
+++ src/Components/Sprig/SprigPrototype.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { type HTMLAttributes } from "react";
+
+interface SprigProps extends HTMLAttributes<HTMLIFrameElement> {
+  [key: string]: any
+
+  link: string
+  wide?: boolean
+  width?: number | string
+  height?: number | string
+  frameBorder?: number | string
+  allow?: string
+}
+
+export const SprigPrototype ({
+    link,
+    style = {},
+  
+    width = 810,
+    height = 600,
+    frameBorder = 1,
+    allow = "encrypted-media",
+    ...props
+  }: SprigProps) => {
+    const url = new URL(link);
+    url.pathname = url.pathname.replace(/\/intl-\w+\//, "/");
+
+
+    return (
+    <>
+    <iframe 
+    src={`https://www.figma.com/embed${url.pathname}`}width={width}
+    height={height}
+    frameBorder={frameBorder}
+    allow={allow}
+    style={{
+      borderRadius: 8,
+      ...style
+    }}
+    {...props}
+  />
+    </>
+    
+);
+
+};
