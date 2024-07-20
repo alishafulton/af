e5d4fe8f0f6c8376247af860520a469a83f46c6a
alishafulton <af.ui.design@gmail.com>
2024-07-20 17:09:59 -0500

    q

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/name.png';
+import logo2 from '../../assets/namehover.png';
+import "./Logo.css";
+
+
+export default function Logo () {
+    return (
+        <>
+        <div class="img-logo"></div></>
+    )
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './Navbar.css';
-import logo from '../../assets/name.png'
 import { Link } from "react-router-dom";
+import Logo from "../Logo/Logo";
 
 export default function Navbar () {
     return (
       <div className='navbar'>
-        <img src={ logo } alt="" className="logo"/>
+        <Logo/>
 
         <ul className="nav-menu">
         <li><Link to="/af/"><h2>home</h2></Link></li>
