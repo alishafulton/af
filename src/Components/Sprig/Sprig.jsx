276a4d3fc3adbda9d1dba2f1c5322c1f22093552
alishafulton <af.ui.design@gmail.com>
2024-07-20 10:58:54 -0500

    i

--- src/Components/Sprig/Sprig.jsx
+++ src/Components/Sprig/Sprig.jsx
@@ -10,7 +10,7 @@ export default function Sprig () {
         <img src={ sprig1 } alt="" className="pimg" />
 
         <div className="flex flex-row gap-8">
-              <SprigPrototype link="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F4Sc1LEgwnnOIf8Z31dkYtD%2FSPRIG%3Fpage-id%3D34%253A2%26node-id%3D230-347%26viewport%3D369%252C25%252C0.26%26t%3DHD5UMDuOwPQIH59t-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D230%253A345" />
+              <SprigPrototype/>
             </div>
 
         <img src={ sprig2 } alt="" className="pimg"/>
--- src/Components/Sprig/SprigPrototype.tsx
+++ src/Components/Sprig/SprigPrototype.tsx
@@ -17,19 +17,17 @@ export const SprigPrototype = ({
     style = {},
   
     width = 810,
-    height = 600,
+    height = 450,
     frameBorder = 1,
     allow = "encrypted-media",
 
   }: SprigProps) => {
-    const url = new URL(link);
-    url.pathname = url.pathname.replace(/\/intl-\w+\//, "/");
 
 
     return (
     
     <iframe 
-    src={`https://www.figma.com/embed${url.pathname}`}
+    src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F4Sc1LEgwnnOIf8Z31dkYtD%2FSPRIG%3Fpage-id%3D34%253A2%26node-id%3D230-347%26viewport%3D369%252C25%252C0.26%26t%3DHD5UMDuOwPQIH59t-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D230%253A345" 
     width={width}
     height={height}
     frameBorder={frameBorder}
@@ -38,5 +36,6 @@ export const SprigPrototype = ({
     
     
 );
+
 
 };
