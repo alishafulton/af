0d7360ba7524aa220526ea9daa2ad96e70a8aec0
alishafulton <af.ui.design@gmail.com>
2024-07-19 19:55:09 -0500

    r

--- src/Components/pic3/pic3.jsx
+++ src/Components/pic3/pic3.jsx
@@ -6,8 +6,8 @@ export default function ComboPics () {
     return (
         <div className="bkgd2">
 
-            This would normally be the part of the story where I tell you
-            what an absolute mess it was and we all laugh... except it wasn't a mess.
+            <div className="wtxt">This would normally be the part of the story where I tell you
+            what an absolute mess it was and we all laugh... except it wasn't a mess.</div>
 
             <img src={ pic10 } alt="" className="combo2"/>
         </div>
