b761e54a6c1b6622218b0b7ab014e35d5c48a2d9
alishafulton <af.ui.design@gmail.com>
2024-07-19 20:58:24 -0500

    v

--- src/Components/BigPic/BigPic2.jsx
+++ src/Components/BigPic/BigPic2.jsx
@@ -8,7 +8,7 @@ export default function BigPic2 () {
         <div className="wtxt2">Unfortunately the heat is worse than the bugs in the summer, so we had 
         to take a sabbatical until Texas cools off!</div>
 
-        <img src={ pic13 } alt="" className="bigpic"/>
+        <img src={ pic13 } alt="" className="bigpic2"/>
         
         </div>
     );
