e86252d3fda31a2c1e3a66817cc862402be808bf
alishafulton <af.ui.design@gmail.com>
2024-07-19 21:03:54 -0500

    x

--- src/Components/BigPic/BigPic2.jsx
+++ src/Components/BigPic/BigPic2.jsx
@@ -5,8 +5,8 @@ export default function BigPic2 () {
     return (
         <div className="bkgd3">
             
-        <div className="wtxt2">Unfortunately the heat is worse than the bugs in the summer, so we had 
-        to take a sabbatical until Texas cools off!</div>
+        <div className="wtxt2">Unfortunately, the heat is worse than the bugs in the summer, so we had 
+        to take a small sabbatical until Texas cools off!</div>
 
         <img src={ pic13 } alt="" className="bigpic2"/>
         
