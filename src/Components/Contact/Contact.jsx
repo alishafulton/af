b735ce6cd34a11e1e7db54162679629e7999e948
alishafulton <af.ui.design@gmail.com>
2024-07-20 19:18:47 -0500

    f

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
+
+export default function ContactForm() {
+  return (
+    <div className="Contact">
+      <h1>Contact Me!</h1>
+      <ContactForm />
+    </div>
+  );
+}
