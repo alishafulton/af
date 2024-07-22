e6a9143cfa9fc5c42c60c79ee9b93e025f3b18c7
alishafulton <af.ui.design@gmail.com>
2024-07-22 15:47:40 -0500

    r

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     './src/components/**/*.{html,js,jsx,ts,tsx}', 
-  './src/pages/**/*.{html,js,jsx,ts,tsx}', ],
+  './src/pages/**/*.{html,js,jsx,ts,tsx}', 
+'./src/components/**/*.jsx'],
   theme: {
     extend: {},
   },
