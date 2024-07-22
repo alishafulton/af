2d2aa45e3998de9fd0534d0df08a3ef10c3815bd
alishafulton <af.ui.design@gmail.com>
2024-07-22 09:05:42 -0500

    Update ContactPage.jsx

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ContactPage.css'
-import ContactForm from './ContactForm.jsx';
 
 
 export default function ContactPage() {
   return (
     <div className="Contact">
+
+      
     </div>
   );
 }
