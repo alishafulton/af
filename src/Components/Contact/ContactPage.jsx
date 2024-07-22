de2a6bbf25238b4efc1a8fb919215d2189751b03
alishafulton <af.ui.design@gmail.com>
2024-07-22 09:50:00 -0500

    z

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './ContactPage.css'
 import ContactForm from './ContactForm';
-
+import behance from '../../assets/behance.png'
+import linkedin from '../../assets/linkedin.png'
 
 export default function ContactPage() {
   return (
     <div className="Contact">
       <ContactForm/>
+
+      <br/>
+      Additional Links: <br/>
+
+      <img src={ behance } alt="" className="logopic" />
+      <img src={ linkedin } alt="" className="logopic" />
 
     </div>
   );
