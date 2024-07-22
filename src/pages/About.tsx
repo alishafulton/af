560bc9e5537a3b4bcb965a29f9c4d049597b3880
alishafulton <af.ui.design@gmail.com>
2024-07-22 12:35:48 -0500

    k

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ import AboutPage from "../Components/AboutPage/AboutPage";
   export default function About () {
     return (
 
-      <div>
+      <div className="text-green-300">
         <AboutPage />
         <Outlet/>
       </div>
