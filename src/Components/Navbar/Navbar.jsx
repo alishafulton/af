2fe25143a9c139d02bbf19ffd1d03bb0d03ee03d
alishafulton <af.ui.design@gmail.com>
2024-07-23 16:41:28 -0500

    e

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import Logo from "../Logo/Logo";
 
 export default function Navbar () {
     return (
-      <div className="justify-center bg-gray-900">
+      <div className="justify-center">
         <Logo/>
 
         <div class="min-h-screen flex place-content-start tan-color font-serif">
-        <li><Link to="/af/"><h2>about me</h2></Link></li>
+        <div className="text-lg"><Link to="/af/"><h2>about me</h2></Link></div>
         <li><Link to="/af/portfolio"><h2>portfolio</h2></Link></li>
         <li><Link to="/af/contact"><h2>contact</h2></Link></li>
         </div>
