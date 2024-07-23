35c5e8ffa7c7be2f41d5aa41552e31eecd812f6a
alishafulton <af.ui.design@gmail.com>
2024-07-23 13:54:21 -0500

    t

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import './index.css'
-import { Garamond } from '../public/fonts/AppleGaramond.ttf'
+import Garamond from './assets/AppleGaramond.ttf';
  
 const garamond = Garamond({
   variable: "--font-garamond",
