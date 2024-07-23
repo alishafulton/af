597e62436d45c76457a48b31db1255c58137b31d
alishafulton <af.ui.design@gmail.com>
2024-07-23 16:06:58 -0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        serif: ["Garamond", ...fontFamily.serif],
+        serif: ["Garamond", "serif"],
     },
   },
   variants: {
