eb6aa9534d9253cb9351c65099d930088f027b93
alishafulton <af.ui.design@gmail.com>
2024-07-24 16:12:10 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import Logo from "../Logo/Logo";
 
 export default function Navbar () {
   const activeLink = "text-blue underline";
+
     return (
       <div>
         <div className="flex justify-center"><Logo/></div>
@@ -12,12 +13,9 @@ export default function Navbar () {
         
         <div className="flex justify-between pb-2 grow font-sans text-2xl tracking-tighter p-0.5 text-black">
 
-        <div><NavLink 
-          to="/af/" 
-          className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink></div>
-
-        <div><NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink></div>
-        <div><NavLink to="/af/contact" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink></div>
+        <NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
+        <NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink>
+        <NavLink to="/af/contact" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink>
         </div>
         
         
