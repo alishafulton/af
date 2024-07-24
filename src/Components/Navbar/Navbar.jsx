913889bcf70771d0a7fbdb7e868a719d603180ec
alishafulton <af.ui.design@gmail.com>
2024-07-24 14:54:22 -0500

    3

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,9 +3,13 @@ import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
 
+const NavBar = () => {
+  const [activeItem, setActiveItem] = useState("About");
 
-export default function Navbar () {
-  
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
+
     return (
       <div>
         <div className="flex justify-center"><Logo/></div>
@@ -13,12 +17,11 @@ export default function Navbar () {
         
         <div className="flex justify-between pb-2">
 
-        <div><NavLink to="/af/" className={({ isActive }) => [
-          "grow font-sans text-2xl tracking-tighter p-0.5",
-          isActive ? "text-black" : "text-blue"
-        ].join("") 
-      }
-        >about me</NavLink></div>
+        <div><NavLink to="/af/" onClick={() => handleItemClick("About")}
+              className={`text-black ${
+                activeItem === "About" && "font-bold text-red-600"
+              }`}>about me</NavLink></div>
+              
         <div className="grow font-sans text-2xl tracking-tighter p-0.5 text-blue"><NavLink to="/af/portfolio">portfolio</NavLink></div>
         <div className="grow font-sans text-2xl tracking-tighter p-0.5"><NavLink to="/af/contact">contact</NavLink></div>
         </div>
@@ -28,4 +31,5 @@ export default function Navbar () {
       
     )
   };
-  
+
+export default NavBar;
--- src/Components/Text/text.jsx
+++ src/Components/Text/text.jsx
@@ -17,7 +17,7 @@ export default function Text () {
     <p>Working as a barista at a Starbucks in Santa Barbara, California, I 
     started taking Computer Science courses online. I used as many free or 
     inexpensive resources that I could to continue learning different coding
-    languages, helpful tools and programs like Visual Studio Code. While my head was 
+    languages, helpful tools and programs like Tailwind CSS and Visual Studio Code. While my head was 
     in my computer in my free time, I spent my mornings growing into a Shift 
     Supervisor at Starbucks. I moved from state to state, transferring stores
     and re-learning an entire new group of people (both baristas and customers!)</p> <br/>
