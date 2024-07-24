63458e3195f269eb56a3f51598f445c74ed35a1c
alishafulton <af.ui.design@gmail.com>
2024-07-24 06:11:37 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,13 +6,13 @@ export default function Navbar () {
   
     return (
       <div className="flex justify-center">
-        <div><Logo/></div>
+        <div><Logo/></div><br/>
 
-        <div class="min-h-screen flex place-content-start tan-color font-serif">
+        
         <div className="text-lg"><Link to="/af/"><h2>about me</h2></Link></div>
         <div className="text-lg"><Link to="/af/portfolio"><h2>portfolio</h2></Link></div>
         <div className="text-lg"><Link to="/af/contact"><h2>contact</h2></Link></div>
-        </div>
+        
         
         </div>
       
