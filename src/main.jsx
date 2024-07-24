6a6f0646ad4a99e7201ecfdb907910fc3712f16b
alishafulton <af.ui.design@gmail.com>
2024-07-24 16:02:12 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/af/",
+        path: "/af/about",
         element: <AboutPage />,
       },
       {
