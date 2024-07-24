577f84416af53ca1e079c57ff4dd2e70ee79dc1c
alishafulton <af.ui.design@gmail.com>
2024-07-24 05:49:29 -0500

    p

--- src/Components/Logo/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import logo from "../../assets/name.png"
-
-export default function Logo () {
-    return (
-        <div><img src={ logo }/></div>
-    )
-}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../Logo/Logo";
-import Dropdown from "../Dropdown/Dropdown";
+import { logo } from "../../assets/name.png"
 
 export default function Navbar () {
-
+  
     return (
-      <>
-        <div className="flex justify-center"><Logo/></div>
-        <div className="flex justify-center"><Dropdown/></div>
+      <div className="flex justify-center">
+        <div><img src={ logo }/></div>
 
+        <div class="min-h-screen flex place-content-start tan-color font-serif">
+        <div className="text-lg"><Link to="/af/"><h2>about me</h2></Link></div>
+        <div className="text-lg"><Link to="/af/portfolio"><h2>portfolio</h2></Link></div>
+        <div className="text-lg"><Link to="/af/contact"><h2>contact</h2></Link></div>
+        </div>
         
-        </>
+        </div>
       
     )
   };
