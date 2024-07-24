d32a847dd0e85634ec2945bef6ace40c0b48815c
alishafulton <af.ui.design@gmail.com>
2024-07-23 19:21:08 -0500

    q

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
+import Dropdown from "../Dropdown/Dropdown";
 
 export default function Navbar () {
+
     return (
       <div className="flex justify-center">
         <div><Logo/></div>
+        <Dropdown/>
 
-        <div class="min-h-screen flex place-content-start tan-color font-serif">
-        <div className="text-lg"><Link to="/af/"><h2>about me</h2></Link></div>
-        <li><Link to="/af/portfolio"><h2>portfolio</h2></Link></li>
-        <li><Link to="/af/contact"><h2>contact</h2></Link></li>
-        </div>
         
         </div>
       
