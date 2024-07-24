2c6589357eaa6764fb98375b4b8ce8d1422f3e66
alishafulton <af.ui.design@gmail.com>
2024-07-24 15:40:08 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,25 +3,20 @@ import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
 
-const NavBar = () => {
-  const [activeItem, setActiveItem] = useState("About");
 
-  const handleItemClick = (item) => {
-    setActiveItem(item);
-  };
-
+export default function Navbar () {
+  const activeLink = "text-blue";
     return (
       <div>
         <div className="flex justify-center"><Logo/></div>
 
         
-        <div className="flex justify-between pb-2">
+        <div className="flex justify-between pb-2 grow font-sans text-2xl tracking-tighter p-0.5 text-black active:text-blue">
 
-        <div><NavLink to="/af/" onClick={() => handleItemClick("About")}
-              className={`text-black ${
-                activeItem === "About" && "font-bold text-red-600"
-              }`}>about me</NavLink></div>
-              
+        <NavLink 
+          to="/af/" 
+          className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
+
         <div className="grow font-sans text-2xl tracking-tighter p-0.5 text-blue"><NavLink to="/af/portfolio">portfolio</NavLink></div>
         <div className="grow font-sans text-2xl tracking-tighter p-0.5"><NavLink to="/af/contact">contact</NavLink></div>
         </div>
@@ -31,5 +26,4 @@ const NavBar = () => {
       
     )
   };
-
-export default NavBar;
+  
