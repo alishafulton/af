8a76cce2b30f257aaa771d89922d38bd86785c72
alishafulton <af.ui.design@gmail.com>
2024-07-25 12:59:32 -0500

    Update ContactPage.jsx

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -5,8 +5,9 @@ import Links from './Links';
 export default function ContactPage() {
   return (
 
-    <div className="justify-center pt-4 text-2xl tracking-tighter">
-      Send me a message!
+    <div className="flex justify-center pt-4"> 
+    <div className="text-2xl tracking-tighter">
+      Send me a message!</div>
         <div className="p-2">
           <ContactForm/>
         </div>
