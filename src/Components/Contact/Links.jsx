6a1b36460ec12513fadb1b486ef32568e018a183
alishafulton <af.ui.design@gmail.com>
2024-07-25 15:06:29 -0500

    Update Links.jsx

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -8,7 +8,7 @@ export default function Links() {
     <div className="pt-1 ">
     <div className="flex justify-center font-sans text-lg tracking-tighter"> Additional Links: </div>
 
-        <div className="w-1/2 justify-between content-start pt-2">
+        <div className="w-1/2 justify-between content-start pt-1">
         <Linkedin/><Behance/>
 </div></div>
   );
