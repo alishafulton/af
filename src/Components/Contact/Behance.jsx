05e66f50504a9a9d7a213a322bf1b16cf084e0bf
alishafulton <af.ui.design@gmail.com>
2024-07-25 10:53:28 -0500

    Update Behance.jsx

--- src/Components/Contact/Behance.jsx
+++ src/Components/Contact/Behance.jsx
@@ -4,7 +4,7 @@ import behance from '../../assets/behance.png'
 export default function Behance() {
   return (
     <>
-         <a href="https://www.behance.net/alishaf2" className="flex px-4">
+         <a href="https://www.behance.net/alishaf2" className="flex h-3 w-3 px-4">
                 <img src={ behance } alt="" />
             </a>
     </>
