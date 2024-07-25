dbf927e4376db07a5a11ceb73978ad02843113f0
alishafulton <af.ui.design@gmail.com>
2024-07-25 10:07:54 -0500

    Update Links.jsx

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -5,9 +5,9 @@ import linkedin from '../../assets/linkedin.png'
 export default function Links() {
   return (
     <>
-    <div className="flex justify-center font-sans text-base tracking-tighter h-20"> Additional Links: </div><br/>
+    <div className="flex justify-center font-sans text-base tracking-tighter h-12"> Additional Links: </div><br/>
 
-        <div className="flex px-6 justify-between">
+        <div className="flex justify-between content-start">
             <a href="https://www.behance.net/alishaf2" className="flex px-4">
                 <img src={ behance } alt="" />
             </a>
