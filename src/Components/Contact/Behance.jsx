88e5dd6f1edda0def096709f96302c2d3d14c174
alishafulton <af.ui.design@gmail.com>
2024-07-25 16:10:50 -0500

    n

--- src/Components/Contact/Behance.jsx
+++ src/Components/Contact/Behance.jsx
@@ -3,7 +3,7 @@ import behance from '../../assets/behance.png'
 
 export default function Behance() {
   return (
-    <div className="p-2">
+    <div className="p-2" filter drop-shadow-lg>
          <a href="https://www.behance.net/alishaf2">
                 <img src={ behance } alt="" className="h-20 w-20"/>
           </a>
--- src/Components/Contact/Linkedin.jsx
+++ src/Components/Contact/Linkedin.jsx
@@ -3,7 +3,7 @@ import linkedin from '../../assets/linkedin.png'
 
 export default function Linkedin() {
   return (
-    <div className="p-2">
+    <div className="p-2 filter drop-shadow-lg">
         <a href="https://www.linkedin.com/in/alishafulton/">
             <img src={ linkedin } alt="" className="h-20 w-20" />
         </a>
