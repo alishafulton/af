a4fa42b2a75aa4ff507255b4bd0c0f7fa24bc894
alishafulton <af.ui.design@gmail.com>
2024-07-25 11:21:04 -0500

    x

--- src/Components/Contact/Linkedin.jsx
+++ src/Components/Contact/Linkedin.jsx
@@ -3,7 +3,7 @@ import linkedin from '../../assets/linkedin.png'
 
 export default function Linkedin() {
   return (
-    <div className="p-2">
+    <div className="p-5">
         <a href="https://www.linkedin.com/in/alishafulton/">
             <img src={ linkedin } alt="" />
         </a>
