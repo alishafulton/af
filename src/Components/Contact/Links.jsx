4ed97d0f0bb2045c4fc29b68b44d770d55bcc664
alishafulton <af.ui.design@gmail.com>
2024-07-25 11:35:22 -0500

    Update Links.jsx

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -5,11 +5,11 @@ import Behance from './Behance';
 
 export default function Links() {
   return (
-    <>
-    <div className="flex justify-center font-sans text-base tracking-tighter"> Additional Links: </div><br/>
+    <div className="pt-2">
+    <div className="flex justify-center font-sans text-base tracking-tighter"> Additional Links: </div>
 
         <div className="flex justify-between content-start mx-8">
         <Linkedin/><Behance/>
-</div></>
+</div></div>
   );
 }
