7bcb9bf01fb3dac420f99868ee08249fa8b88f9b
alishafulton <af.ui.design@gmail.com>
2024-07-27 08:06:49 -0500

    extra title and sprig proto

--- src/Components/BigPic/BigPic.jsx
+++ src/Components/BigPic/BigPic.jsx
@@ -2,6 +2,6 @@ import before from '../../assets/IMG_0549.png'
 
 export default function BigPic () {
     return (
-        <div className="headliner"><div className="headline-text">March 2024</div><img src={ before } alt="" className="bigpic"/></div>
+        <div className="mt-8"><div className="text-3xl">March 2024</div><img src={ before } alt="" className="m-6"/></div>
     );
 };
