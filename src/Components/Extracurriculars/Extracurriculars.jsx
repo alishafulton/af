abda02eff24f1585fa487a7abe41a017143e399d
alishafulton <af.ui.design@gmail.com>
2024-07-28 13:25:00 -0500

    Update Extracurriculars.jsx

--- src/Components/Extracurriculars/Extracurriculars.jsx
+++ src/Components/Extracurriculars/Extracurriculars.jsx
@@ -24,7 +24,7 @@ export default function Extra () {
         
       <div className="flex-col w-full mt-2 px-3">
 
-        <img src={ wood } alt="" className="w-5/12 float-left mr-2 bg-green border-r-2 border-pink  "/>  
+        <img src={ wood } alt="" className="w-5/12 float-left mr-2 bg-green border-r-4 border-purple  "/>  
 
         <div className="font-serif text-lg"> After spending a couple of weeks getting 
         swarmed by crane flies, my cousin and I started 
@@ -34,7 +34,7 @@ export default function Extra () {
         only worsened the situation. </div>
         <br/>
     
-        <img src={ girls } alt="" className="w-5/12 float-right ml-2 mb-1"/>
+        <img src={ girls } alt="" className="w-5/12 float-right border-l-4 border-purple ml-2 mb-1"/>
 
         <div className="font-serif text-lg"> 
         Their porch was already covered with a roof held up by
@@ -45,7 +45,7 @@ export default function Extra () {
         could be the most efficient, we picked a day to get started.</div>
         <br />
         
-        <img src={ pic5 } alt="" className="w-5/12 float-left border-r-2 border-pink "/>
+        <img src={ pic5 } alt="" className="w-5/12 float-left border-r-4 border-purple "/>
         <div className="font-serif text-lg"> Brimming with excitement, we measured 
         everything three times and wrote down exactly what we needed 
         to buy on our first trip to Home Depot. We borrowed an SUV to go back and forth to the store and bought
@@ -53,7 +53,7 @@ export default function Extra () {
         one of three trips ended up being more like five (or six).<br/></div>
 
         
-        <img src={ pic11 } alt="" className="w-5/12 float-right"/>
+        <img src={ pic11 } alt="" className="w-5/12 float-right border-l-4 border-purple"/>
 
         <div className="font-serif text-lg">Now, this would normally be the part of the story where I tell you
         what an absolute mess it was and we all laugh... except it wasn't a mess.</div>
@@ -65,7 +65,7 @@ export default function Extra () {
         patient Home Depot employees, and our iPhone calculators,
         we did it and we did it well. </div><br/>
          
-        <img src={ pic10 } alt="" className="w-5/12 float-left border-r-2 border-pink "/>
+        <img src={ pic10 } alt="" className="w-5/12 float-left border-r-4 border-purple "/>
         
         <BigPic2/>
 
