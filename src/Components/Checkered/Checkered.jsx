ce625746716f771c38ecbac0f5d9ec19ce39df83
alishafulton <af.ui.design@gmail.com>
2024-07-28 15:41:17 -0500

    Update Checkered.jsx

--- src/Components/Checkered/Checkered.jsx
+++ src/Components/Checkered/Checkered.jsx
@@ -4,6 +4,7 @@ export default function Checkered () {
 return (
 <>
 <div className="h-8 my-2 flex">
+    <div>
           <div className="w-2 h-2 bg-transparent"></div>
           <div className="w-2 h-2 bg-orange"></div>
           <div className="w-2 h-2 bg-transparent"></div>
@@ -28,6 +29,80 @@ return (
           <div className="w-2 h-2 bg-orange"></div>
           <div className="w-2 h-2 bg-transparent"></div>
           <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+      </div>
+    <div>
+        <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+          <div className="w-2 h-2 bg-transparent"></div>
+          <div className="w-2 h-2 bg-orange"></div>
+    </div>
       </div>
 </>
 );
