47389c58827d73df2b30bd0003b046f87623598c
alishafulton <af.ui.design@gmail.com>
2024-07-28 18:37:25 -0500

    Update Checkered.jsx

--- src/Components/Checkered/Checkered.jsx
+++ src/Components/Checkered/Checkered.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Checkered () {
 return (
 <>
-<div className="h-6 flex-col -mt-12">
+<div className="h-6 flex-col">
     <div className="flex p-0 m-0">
           <div className="w-2 h-2 bg-transparent"></div>
           <div className="w-2 h-2 bg-orange"></div>
