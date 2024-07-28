d32b17d1916e59c498e3ed9ae68c774c07515e58
alishafulton <af.ui.design@gmail.com>
2024-07-28 15:39:38 -0500

    h

--- src/Components/BigPic/BigPic2.jsx
+++ src/Components/BigPic/BigPic2.jsx
@@ -3,7 +3,7 @@ import pic13 from '../../assets/IMG_0967.png'
 export default function BigPic2 () {
     return (
         <div>
-         <div className="mb-2">   
+         <div className="mb-8">   
         <div className="font-serif text-lg">
             Unfortunately, the heat is worse 
             than the bugs in the summer, so we 
