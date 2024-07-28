1aafea7f02f4e2dd8a254f0e22c83112afb9b990
alishafulton <af.ui.design@gmail.com>
2024-07-28 09:40:39 -0500

    Update BigPic2.jsx

--- src/Components/BigPic/BigPic2.jsx
+++ src/Components/BigPic/BigPic2.jsx
@@ -2,16 +2,16 @@ import pic13 from '../../assets/IMG_0967.png'
 
 export default function BigPic2 () {
     return (
-        <div className="py-2">
-            
-        <div className="font-serif">
+        <div>
+         <div className="mb-2">   
+        <div className="font-serif text-lg">
             Unfortunately, the heat is worse 
             than the bugs in the summer, so we 
             had to take a small sabbatical 
             until Texas cools off!
-        </div>
+        </div></div>
 
-        <img src={ pic13 } alt="" className="h-30"/>
+        <img src={ pic13 } alt="" />
         
         </div>
     );
