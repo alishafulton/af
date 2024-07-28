c59a43d22342b99e1c083e925a9e586a68372b34
alishafulton <af.ui.design@gmail.com>
2024-07-28 18:12:38 -0500

    2

--- src/Components/BigPic/BigPic.jsx
+++ src/Components/BigPic/BigPic.jsx
@@ -8,8 +8,9 @@ export default function BigPic () {
                 <div class="text-overlay absolute left-0 top-0 p-2 font-sans text-white text-4xl">
 
                     March 2024
-                <div className="-mt-3"><Checkered /></div>
+                
                 </div>
+            <div className="-mt-3"><Checkered /></div>
         </div>
     );
 };
