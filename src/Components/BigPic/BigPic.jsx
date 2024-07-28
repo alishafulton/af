c06bcc435fe94dae01c67eb75bc2d03d49c798eb
alishafulton <af.ui.design@gmail.com>
2024-07-28 12:47:42 -0500

    Update BigPic.jsx

--- src/Components/BigPic/BigPic.jsx
+++ src/Components/BigPic/BigPic.jsx
@@ -3,7 +3,7 @@ import before from '../../assets/IMG_0549.png'
 export default function BigPic () {
     return (
         <div className="image-container relative mt-6 mx-3">
-            <img src={before} alt="" className="w-full" />
+            <img src={before} alt="" className="w-full h-full" />
                 <div class="text-overlay absolute left-0 top-0 p-2 font-sans text-white text-4xl">
                     March 2024
                 </div>
