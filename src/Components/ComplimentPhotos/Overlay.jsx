fa6070dac04fe907110c2792b9acc4ba8a0593aa
alishafulton <af.ui.design@gmail.com>
2024-07-29 16:21:04 -0500

    Update Overlay.jsx

--- src/Components/ComplimentPhotos/Overlay.jsx
+++ src/Components/ComplimentPhotos/Overlay.jsx
@@ -6,7 +6,7 @@ export function Overlay({ isOpen, onClose, children }) {
       {isOpen && (
         <div className="flex justify-center">
           <div className="bg-black opacity-80 w-screen h-screen fixed top-0 left-0 z-9 cursor-pointer" onClick={onClose}/>
-          <div className="bg-pink justify-center z-10 w-fit h-fit" >
+          <div className="bg-pink fixed top-0 bottom-0 left-0 right-0 m-auto p-30 z-10 w-fit h-fit" >
             {children}
           </div>
         </div>
