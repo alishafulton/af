6ba7b54cdf0eec8f0d8b5d91daec18cb44497de7
alishafulton <af.ui.design@gmail.com>
2024-07-29 16:36:32 -0500

    d

--- src/Components/Blurb/Compliments.jsx
+++ src/Components/Blurb/Compliments.jsx
@@ -37,7 +37,7 @@ export default function Compliments () {
         </button>
 
           <Overlay isOpen={isOpen} onClose={toggleOverlay}>
-          <div className="flex justify-center items-center h-screen bg-black">
+          <div className="flex justify-center items-center h-screen bg-transparent">
           <div className="max-w-lg">
                 <Carousel autoSlide={false}>
                    {[...slides.map((s) => (<img src={s} />)),]}
