b311c138613798ceccdcf7d1221c1ac38a0b93ed
alishafulton <af.ui.design@gmail.com>
2024-07-29 14:48:37 -0500

    Update Carousel.jsx

--- src/Components/Blurb/Carousel.jsx
+++ src/Components/Blurb/Carousel.jsx
@@ -32,7 +32,7 @@ const Carousel = ({ children: slides, autoSlide = false, autoSlideInterval = 300
             </div>
 
 
-            <div className='absolute bottom-4 right-0 left-0'>
+            <div className='absolute bottom-2 right-0 left-0'>
                 <div className='flex items-center justify-center gap-2'>
                     {slides.map((s, i) => (
                         <div key={i} className={`transition-all w-1.5 h-1.5 bg-white rounded-full  ${curr === i ? "p-0.5" : "bg-opacity-50"}`} />
