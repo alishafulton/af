c63fa6c8a4a06d2f35a697699b3c9451d3129a16
alishafulton <af.ui.design@gmail.com>
2024-07-29 11:28:16 -0500

    Update Overlay.jsx

--- src/Components/ComplimentPhotos/Overlay.jsx
+++ src/Components/ComplimentPhotos/Overlay.jsx
@@ -6,10 +6,10 @@ export function Overlay({ isOpen, onClose, children }) {
       {isOpen && (
         <div className="overlay">
           <div className="bg-black opacity-50 w-screen h-screen fixed top-0 left-0 z-9 cursor-pointer" onClick={onClose} />
-          <div className="bg-white fixed top-0 bottom-0 left-0 right-0 z-10 p-30 w-2/3 h-2/3 m-auto">
+          <div className="bg-white fixed top-0 bottom-0 left-0 right-0 m-auto z-10 p-7 w-fit h-fit ">
             <div className="flex justify-end align-middle">
               <button
-                className="overlay__close"
+                className="cursor-pointer bg-transparent"
                 type="button"
                 onClick={onClose}
               />
