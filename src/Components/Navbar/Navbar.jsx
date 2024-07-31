39476f3eda8af8ddf82703fabb8ccedda5782ce3
alishafulton <af.ui.design@gmail.com>
2024-07-31 16:54:30 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar () {
         <div className="w-full justify-center lg:col-span-3 lg:col-start-2 mb-1"><Logo/></div>
 
         
-        <div className="lg:col-span-3 lg:col-start-2 my-3">
+        <div className="lg:col-span-3 lg:col-start-2 my-1 lg:my-2 ">
           <div className="grid grid-cols-3 font-sans text-3xl lg:text-5xl tracking-tighter text-black">
 
         <div className="justify-self-start"><NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink></div>
