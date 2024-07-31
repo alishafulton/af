cbfd22bbf764f4bd0e2305a2e54d7996bd47f0ea
alishafulton <af.ui.design@gmail.com>
2024-07-31 13:40:31 -0500

    b

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 export default function App () {
   return (
-    <div className=" justify-center">
+    <div className="flex w-full justify-center">
         <Navbar className="mt-6"/>
         <Outlet/>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,19 +7,17 @@ export default function Navbar () {
   const activeLink = "text-purple";
 
     return (
-      <div className="w-full lg:w-2/3 justify-center ">
-        <div className="flex content-start mt-3 justify-center lg:max-w-2/3"><Logo/></div>
+      <div className="flex w-full lg:w-2/3 justify-center">
 
-        
-        <div className="flex grow justify-between lg:max-w-2/3 pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
+        <div className="flex mt-3 "><Logo/></div>
 
-        <NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
-        <NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink>
-        <NavLink to="/af/contact" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink>
+        <div className="flex justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
+          <NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
+          <NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink>
+          <NavLink to="/af/contact" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink>
         </div>
         
-        
-        </div>
+      </div>
       
     )
   };
