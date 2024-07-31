f8d461ad86ab01581c68b2a4b6bba91f3e33b80d
alishafulton <af.ui.design@gmail.com>
2024-07-31 14:57:02 -0500

    o

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar () {
   const activeLink = "text-purple";
 
     return (
-      <div className="flex-col w-2/3 justify-center mx-1">
+      <div className="flex-col justify-center mx-1">
 
         <div className="flex mt-3 w-full justify-center"><Logo/></div>
 
