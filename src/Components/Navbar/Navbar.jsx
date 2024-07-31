efa37200836c78489a1ee724c52f75e203879ca4
alishafulton <af.ui.design@gmail.com>
2024-07-31 14:15:28 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar () {
     return (
       <div className="flex-col w-full justify-center">
 
-        <div className="flex mt-3 "><Logo/></div>
+        <div className="flex mt-3 w-full justify-center"><Logo/></div>
 
         
         <div className="flex grow justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
