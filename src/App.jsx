2d964cf59d702115d71c13f07e56a75ac51b6ad0
alishafulton <af.ui.design@gmail.com>
2024-07-31 14:05:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 export default function App () {
   return (
-    <div className="flex w-screen justify-center">
+    <div className="flex-col w-screen justify-center">
         <Navbar className="mt-6"/>
         <Outlet/>
     </div>
