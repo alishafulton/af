a1389d4437a021f1dbc612417e92307fef87aab7
alishafulton <af.ui.design@gmail.com>
2024-07-31 17:33:46 -0500

    Update Blurb.jsx

--- src/Components/Blurb/Blurb.jsx
+++ src/Components/Blurb/Blurb.jsx
@@ -11,8 +11,8 @@ export default function Blurb () {
 
 
 
-    <div className="justify-center">
-      <Selfie className="w-1/2 float-left pr-3 pb-0.5"/>
+    <div className="justify-center flex">
+      <Selfie className="w-1/2 max-w-sm float-left pr-3 pb-0.5"/>
         
       <Text/>
 
