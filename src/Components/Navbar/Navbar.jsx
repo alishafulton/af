d7f4ec28ad484e50c4227ff358db0655d7320344
alishafulton <af.ui.design@gmail.com>
2024-07-31 15:50:27 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,12 +7,12 @@ export default function Navbar () {
   const activeLink = "text-purple";
 
     return (
-      <div className="flex-col justify-center mx-1">
+      <div className="flex-col justify-center m-1">
 
         <div className="flex mt-3 w-full justify-center"><Logo/></div>
 
         
-        <div className="w-full lg:w-2/3 justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
+        <div className="justify-between w-full lg:w-2/3 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
 
         <NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
         <NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink>
