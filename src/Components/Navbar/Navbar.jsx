8be66fe102dc6f4f90ff258d7cc69ade1562993f
alishafulton <af.ui.design@gmail.com>
2024-07-31 15:46:31 -0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar () {
         <div className="flex mt-3 w-full justify-center"><Logo/></div>
 
         
-        <div className="flex justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
+        <div className="w-full lg:w-2/3 justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
 
         <NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
         <NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink>
