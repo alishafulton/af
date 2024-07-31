c222a7eaa3bd9b3953fc2a63406794af42dbfde0
alishafulton <af.ui.design@gmail.com>
2024-07-31 15:35:12 -0500

    p

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './index.css'
 export default function App () {
   return (
     <div className="flex-col w-screen justify-center">
-       <div className="w-full justify-center"> <Navbar /></div>
+       <div className="w-full justify-center"> <Navbar className="justify-center" /></div>
         <Outlet/>
     </div>
     
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,12 +7,12 @@ export default function Navbar () {
   const activeLink = "text-purple";
 
     return (
-      <div className="flex-col w-2/3 justify-center mx-1">
+      <div className="flex-col justify-center mx-1">
 
         <div className="flex mt-3 w-full justify-center"><Logo/></div>
 
         
-        <div className="flex grow justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
+        <div className="flex justify-between pb-4 font-sans text-3xl lg:text-5xl tracking-tighter text-black px-1 ">
 
         <NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink>
         <NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink>
