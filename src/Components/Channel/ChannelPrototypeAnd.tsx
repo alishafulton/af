9a7b1f77a00b2be4d94d2954f71b1e287de8bc25
alishafulton <af.ui.design@gmail.com>
2024-08-01 10:13:45 -0500

    z

--- src/Components/Channel/ChannelPrototypeAnd.tsx
+++ src/Components/Channel/ChannelPrototypeAnd.tsx
@@ -5,7 +5,7 @@ export default function ChannelPrototypeAnd () {
     <div className="flex mx-12 h-96 justify-center">
   <iframe 
   src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fh3kIgfl3znkN9dbk32kQCu%2FChannel---Android%3Fpage-id%3D0%253A1%26node-id%3D1-6567%26t%3DsIk5LJjvgxfgiiuD-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D1%253A6567" 
-  className="h-full w-1/2"/>
+  className="h-full w-1/2 lg:w-full"/>
   </div>
   );
 };
--- src/Components/Channel/ChannelPrototypeiOS.tsx
+++ src/Components/Channel/ChannelPrototypeiOS.tsx
@@ -6,7 +6,7 @@ export default function ChannelPrototypeiOS () {
     <div className="flex mx-12 h-96 justify-center">
   <iframe 
   src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FsAbUWT1hu2uos2b3MKKJO8%2FChannel%3Fpage-id%3D0%253A1%26node-id%3D25-10147%26viewport%3D805%252C467%252C0.3%26t%3DWq1OimPb36q92G2X-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D25%253A10147" 
-  className="h-full w-1/2"/>
+  className="h-full w-1/2 lg:w-full"/>
   </div>
   );
 };
