45fad68cb796a309d8b7f40f70f3392196be054e
alishafulton <af.ui.design@gmail.com>
2024-08-02 13:58:14 -0500

    p

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -3,14 +3,16 @@ import Linkedin from './Linkedin';
 import Behance from './Behance';
 import Github from './Github';
 
-// Just a container for my links to keep em cute n together
+// Just a container for my links to keep em cute 'n' together
 
 export default function Links() {
   return (
-    <div className="flex flex-col justify-center">
-      <div className="flex justify-center font-serif text-lg lg:text-xl tracking-tight py-2"> Hi, it's me, just readjusting some things here! </div><br/>
+    <div className="flex flex-col justify-center mt-2">
+      <div className="flex justify-center font-serif text-lg lg:text-xl tracking-tight"> Hi, it's me, just readjusting some things here! </div><br/>
 
-        <div className="grid grid-cols-3 justify-items-center">
+
+{/* for the life of me i cant understand why my github icon is not participating  */}
+        <div className="grid grid-cols-3 justify-items-center w-96">
         <div className="flex"><Linkedin/></div>
         <div className="flex"><Behance/></div>
         <div className="flex"><Github/></div>
