8afe348990cea1e52d73af790ed3f06bd18b414a
alishafulton <af.ui.design@gmail.com>
2024-08-02 14:04:02 -0500

    Update Links.jsx

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -7,12 +7,12 @@ import Github from './Github';
 
 export default function Links() {
   return (
-    <div className="flex flex-col justify-center mt-2">
+    <div className="flex flex-col justify-center mt-4">
       <div className="flex justify-center font-serif text-lg lg:text-xl tracking-tight"> Hi, it's me, just readjusting some things here! </div><br/>
 
 
 {/* for the life of me i cant understand why my github icon is not participating  */}
-        <div className="grid grid-cols-3 justify-items-center w-96">
+        <div className="grid grid-cols-3 justify-center w-96">
         <div className="flex"><Linkedin/></div>
         <div className="flex"><Behance/></div>
         <div className="flex"><Github/></div>
