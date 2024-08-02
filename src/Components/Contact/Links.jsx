16f4d11ac617477ffa18fffa63a3fc623e396866
alishafulton <af.ui.design@gmail.com>
2024-08-02 14:10:36 -0500

    Update Links.jsx

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -12,7 +12,7 @@ export default function Links() {
 
 
 {/* for the life of me i cant understand why my github icon is not participating  */}
-        <div className="grid grid-cols-3 justify-center w-96">
+        <div className="justify-center grid grid-cols-3  w-80">
         <div className="flex"><Linkedin/></div>
         <div className="flex"><Behance/></div>
         <div className="flex"><Github/></div>
