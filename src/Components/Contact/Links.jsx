dbd32b2aa734a2de3d7a737a1844d92062d2f2a0
alishafulton <af.ui.design@gmail.com>
2024-08-02 15:40:34 -0500

    Update Links.jsx

--- src/Components/Contact/Links.jsx
+++ src/Components/Contact/Links.jsx
@@ -12,7 +12,7 @@ export default function Links() {
 
 
 {/* for the life of me i cant understand why my github icon is not participating  */}
-        <div className="grid grid-cols-3 m-16">
+        <div className="grid grid-cols-3 m-16 gap-2 w-60">
 <Linkedin/>
 <Behance/>
 <Github/>
