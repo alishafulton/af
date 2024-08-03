14dd3c8d7582a693e86202731d0adf3d5ea93789
alishafulton <af.ui.design@gmail.com>
2024-08-03 14:20:14 -0500

    Update Carousel.jsx

--- src/Components/Blurb/Carousel.jsx
+++ src/Components/Blurb/Carousel.jsx
@@ -33,7 +33,7 @@ const Carousel = ({ children: slides, autoSlide = false, autoSlideInterval = 300
                 {slides}
             </div>
 
-            <div className="absolute inset-0 flex items-center justify-between p-2 mx-8">
+            <div className="absolute inset-0 flex items-center justify-between p-2 mx-16">
                 <button onClick={prev} className='p-3 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white'>
                     <ChevronLeft />
                 </button>
