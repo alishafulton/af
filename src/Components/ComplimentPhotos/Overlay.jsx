657fe2a12dddb7057ff65dfb78c148174a18b2b0
alishafulton <af.ui.design@gmail.com>
2024-08-03 12:17:09 -0500

    Update Overlay.jsx

--- src/Components/ComplimentPhotos/Overlay.jsx
+++ src/Components/ComplimentPhotos/Overlay.jsx
@@ -6,13 +6,13 @@ export function Overlay({ isOpen, onClose, children }) {
     // when the overlay=isopen, the overlay appears!
     // a 60% opacity black background that can be clicked to close
     // and my children (the carousel)
-    
+
     <Fragment>
       {isOpen && (
         <div className="flex">
           <div className="bg-black opacity-60 w-screen h-screen fixed top-0 left-0 z-9 cursor-pointer" onClick={onClose}/>
 
-          <div className="bg-transparent h-2/3 w-2/3 lg:w-1/3 fixed top-2 bottom-2 left-0 right-0 m-auto z-10 " >
+          <div className="bg-white h-2/3 w-2/3 lg:w-1/3 lg:h-1/3 fixed top-2 bottom-2 left-0 right-0 m-auto z-10 " >
             {children}
           </div>
         </div>
