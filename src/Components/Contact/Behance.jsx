52fcf3dad2b46fb5a23bf91861308f1b08a56d2c
alishafulton <af.ui.design@gmail.com>
2024-08-03 10:46:18 -0500

    Update Behance.jsx

--- src/Components/Contact/Behance.jsx
+++ src/Components/Contact/Behance.jsx
@@ -8,7 +8,7 @@ export default function Behance() {
   return (
     <div className="p-1 filter drop-shadow-lg">
          <a href="https://www.behance.net/alishaf2">
-                <img src={ behance } alt="" className="h-full w-full"/>
+                <img src={ behance } alt="" className="h-20 w-20"/>
           </a>
     </div>
   );
