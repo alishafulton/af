4316010ab199a1df87ffcb00d1dd14d3cfa13fcc
alishafulton <af.ui.design@gmail.com>
2024-08-03 16:22:30 -0500

    Update Overlay.jsx

--- src/Components/ComplimentPhotos/Overlay.jsx
+++ src/Components/ComplimentPhotos/Overlay.jsx
@@ -12,7 +12,7 @@ export function Overlay({ isOpen, onClose, children }) {
         <div className="flex">
           <div className="bg-black opacity-60 w-screen h-screen fixed top-0 left-0 z-9 cursor-pointer" onClick={onClose}/>
 
-          <div className=" content-start justify-items-center z-10 bg-white w-1/2 ">
+          <div className=" content-start justify-center z-10 bg-white w-1/2 ">
             <div className="bg-gray-300 justify-center " >
               {children}
             </div>
