9abcc6ba5ac3a07ca65300b270463c5ae61b7db2
alishafulton <af.ui.design@gmail.com>
2024-08-20 10:24:52 -0500

    k

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './index.css'
 export default function App () {
   return (
     <div className="flex-col w-screen justify-items-center p-2.5 md:p-5">
-       <div className="justify-center"> <Navbar className="justify-self-center" /></div>
+       <Navbar/>
         <Outlet/>
     </div>
     
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ return (
    <div className="w-full justify-center lg:col-span-3 lg:col-start-2 mb-1"><Logo/></div>
 
 
-   <div className="w-full max-w-6xl justify-center lg:col-span-3 lg:col-start-2 my-1 lg:my-2 ">
+   <div className="w-full max-w-5xl justify-center lg:col-span-3 lg:col-start-2 my-1 lg:my-2 ">
     <div className="grid grid-cols-3 font-sans text-3xl md:text-4xl xl:text-5xl tracking-tighter text-black">
         <div className="justify-self-start"><NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink></div>
         <div className="justify-self-center"><NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink></div>
