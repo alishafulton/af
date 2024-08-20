e89fbce024735493919803decf95fb608c709912
alishafulton <af.ui.design@gmail.com>
2024-08-20 09:29:13 -0500

    y

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,12 +19,15 @@ export default function Navbar () {
 
 return (
   <div className="flex-col">
-   <div className="w-full justify-center mb-1"><Logo/></div>
+   <div className="w-full justify-center lg:col-span-3 lg:col-start-2 mb-1"><Logo/></div>
 
+
+   <div className="lg:col-span-3 lg:col-start-2 my-1 lg:my-2 ">
     <div className="grid grid-cols-3 font-sans text-3xl md:text-4xl xl:text-5xl tracking-tighter text-black">
         <div className="justify-self-start"><NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink></div>
         <div className="justify-self-center"><NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink></div>
         <div className="justify-self-end"><NavLink to="/af/contact" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink></div>
+    </div>
     </div>
         
         
