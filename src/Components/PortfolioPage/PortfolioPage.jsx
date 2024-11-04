e30a9f8cdd3ad4756108a59574883d50291ea16e
alishafulton <af.ui.design@gmail.com>
2024-11-04 16:35:14 -0600

    b

--- src/Components/PortfolioPage/PortfolioPage.jsx
+++ src/Components/PortfolioPage/PortfolioPage.jsx
@@ -7,7 +7,7 @@ export default function PortfolioPage() {
     
       return (
         <div className="w-full">
-          <div className="w-60 border rounded border-gray-400 ">
+          <div className="w-60 border rounded border-gray-400 justify-center">
           <div className="w-52 ">
             <Mockup/>
           </div>
