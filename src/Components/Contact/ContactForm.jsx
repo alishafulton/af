e94cd8d37092b5f163e2fa5a64be307cf16f35c7
alishafulton <af.ui.design@gmail.com>
2024-11-04 16:37:10 -0600

    x

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -59,7 +59,7 @@ export default function ContactForm() {
             <textarea className="flex-1 border rounded border-black  w-full p-2 h-full " name="message" placeholder="Message" required>
             </textarea></div><br/>
   
-          <div className="flex justify-center "><button type="submit"  className=" w-1/2 h-10 bg-black text-white ">SUBMIT</button>
+          <div className="flex justify-center "><button type="submit"  className=" w-1/3 h-8 bg-black text-white ">SUBMIT</button>
           </div>
         
         </form>
