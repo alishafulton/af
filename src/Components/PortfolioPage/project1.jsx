2da2b85bb90247e3fb26f5c26f97a88dcd32ceba
alishafulton <af.ui.design@gmail.com>
2024-11-04 17:02:39 -0600

    Update project1.jsx

--- src/Components/PortfolioPage/project1.jsx
+++ src/Components/PortfolioPage/project1.jsx
@@ -8,7 +8,7 @@ export default function Project1() {
       return (
 <div className="text-md tracking-wide leading-10 text-gray-400 ">
         <div className="w-64 justify-self-center border border-gray-200">
-          <div className="w-52">
+          <div className="w-52 mx-12">
             <Mockup/>
           </div>
           </div>
