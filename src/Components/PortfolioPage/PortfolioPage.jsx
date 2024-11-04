6fed07da5a6d6d0afd56d66cdd50ef41ed930b3c
alishafulton <af.ui.design@gmail.com>
2024-11-04 15:08:48 -0600

    Update PortfolioPage.jsx

--- src/Components/PortfolioPage/PortfolioPage.jsx
+++ src/Components/PortfolioPage/PortfolioPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-
+import mock1 from "./../../assets/MOCK1.png";
 
 
 
 export default function PortfolioPage() {
     
       return (
-        <div className = "cursor-crosshair ">hello</div>
+        <div className = "w-full cursor-crosshair "><div className="w-8"><mock1/></div></div>
 
       );
 }
