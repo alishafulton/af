a5b825b9054dba898ebd8b99305c3f2368b86e5b
alishafulton <af.ui.design@gmail.com>
2024-11-04 11:47:54 -0600

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,9 +22,9 @@ return (
       
       <div className="w-full max-w-5xl">
         <div className="grid grid-cols-3 font-sans text-3xl md:text-4xl xl:text-5xl tracking-tighter text-black">
-          <div className="justify-self-start"><NavLink to="/af/about" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink></div>
-          <div className="justify-self-center"><NavLink to="/af/portfolio" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink></div>
-          <div className="justify-self-end"><NavLink to="/af/contact" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink></div>
+          <div className="justify-self-start"><NavLink to="/about/" className={({ isActive }) => (isActive ? activeLink : "")}>about me</NavLink></div>
+          <div className="justify-self-center"><NavLink to="/portfolio/" className={({ isActive }) => (isActive ? activeLink : "")}>portfolio</NavLink></div>
+          <div className="justify-self-end"><NavLink to="/contact/" className={({ isActive }) => (isActive ? activeLink : "")}>contact</NavLink></div>
         </div>
     </div>
         
