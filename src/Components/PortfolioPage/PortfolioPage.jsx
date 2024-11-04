a933ba9e068ad08f184d7fd10a41e56e195845f7
alishafulton <af.ui.design@gmail.com>
2024-11-04 17:30:27 -0600

    Update PortfolioPage.jsx

--- src/Components/PortfolioPage/PortfolioPage.jsx
+++ src/Components/PortfolioPage/PortfolioPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Project1 from "./project1";
 import Project2 from "./project2";
 import Project3 from "./project3";
+import Project4 from "./project4";
 
 
 
@@ -15,6 +16,7 @@ export default function PortfolioPage() {
         </div>
         <div className="flex flex-row space-x-6 my-2">
           <Project3 className="m-2" />
+          <Project4 className="m-2" />
         </div>
       </div>
       );
