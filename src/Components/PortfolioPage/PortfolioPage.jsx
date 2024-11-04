d1493767307b748cc22dbfd94b7d4aefdd59b407
alishafulton <af.ui.design@gmail.com>
2024-11-04 17:53:12 -0600

    Update PortfolioPage.jsx

--- src/Components/PortfolioPage/PortfolioPage.jsx
+++ src/Components/PortfolioPage/PortfolioPage.jsx
@@ -3,6 +3,7 @@ import Project1 from "./project1";
 import Project2 from "./project2";
 import Project3 from "./project3";
 import Project4 from "./project4";
+import { NavLink } from "react-router-dom";
 
 
 
@@ -11,7 +12,7 @@ export default function PortfolioPage() {
       return (
         <div className="w-full">
           <div className="flex flex-row space-x-6 my-6">
-          <a href="https://alishafulton.github.io/coast/"><Project1 className="m-2" /></a>
+          <NavLink to="/coast/" className={({ isActive }) => (isActive ? activeLink : "")}><Project1 className="m-2" /></NavLink>
           <Project2 className="m-2" />
         </div>
         <div className="flex flex-row space-x-6 my-6">
