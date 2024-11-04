e484eb19c71ad1016c8ae6057d94efead26d7747
alishafulton <af.ui.design@gmail.com>
2024-11-04 16:44:43 -0600

    Update PortfolioPage.jsx

--- src/Components/PortfolioPage/PortfolioPage.jsx
+++ src/Components/PortfolioPage/PortfolioPage.jsx
@@ -7,8 +7,8 @@ export default function PortfolioPage() {
     
       return (
         <div className="w-full">
-          <div className="w-64 border border-gray-200 justify-center">
-          <div className="w-52 justify-center">
+          <div className="w-64 border border-gray-200">
+          <div className="w-52 justify-items-center">
             <Mockup/>
           </div>
           </div>
