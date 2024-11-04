a7206c7053b891cb0811b8b02b485ae8c7bd0a09
alishafulton <af.ui.design@gmail.com>
2024-11-04 17:00:27 -0600

    Update project1.jsx

--- src/Components/PortfolioPage/project1.jsx
+++ src/Components/PortfolioPage/project1.jsx
@@ -6,8 +6,8 @@ import Mockup from "./mock1";
 export default function Project1() {
     
       return (
-<div className="text-xs tracking-wider">
-        <div className="w-64 border border-gray-200">
+<div className="text-md tracking-wide">
+        <div className="w-64 justify-self-center border border-gray-200">
           <div className="w-52">
             <Mockup/>
           </div>
