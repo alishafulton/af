12515df260eb4de11f1775849b66776c0bd33755
alishafulton <af.ui.design@gmail.com>
2024-11-04 17:08:07 -0600

    oops

--- src/Components/PortfolioPage/project2.jsx
+++ src/Components/PortfolioPage/project2.jsx
@@ -8,7 +8,7 @@ export default function Project2() {
       return (
 <div className="text-md  leading-10 text-black ">
         <div className="w-64 justify-self-center border border-gray-200">
-          <div className="w-52">
+          <div className="w-52 mx-6">
             <Mockup2/>
           </div>
           </div>
