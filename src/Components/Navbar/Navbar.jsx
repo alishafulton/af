db43467af9b94f34655851eca94fc441e7813655
alishafulton <af.ui.design@gmail.com>
2024-11-04 16:18:07 -0600

    v

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ export default function Navbar () {
 
 return (
       
-      <div className="w-full">
-        <div className="flex flex-row font-mono text-xs tracking-tighter text-black">
-          <div className="py-2 px-4 "><NavLink to="/" className={({ isActive }) => (isActive ? activeLink : "")}>PORTFOLIO</NavLink></div>
-          <div className="py-2 px-4 "><NavLink to="/contact/" className={({ isActive }) => (isActive ? activeLink : "")}>CONTACT</NavLink></div>
+      <div className="">
+        <div className="font-mono text-xs tracking-tighter text-black">
+          <div className="p-2"><NavLink to="/" className={({ isActive }) => (isActive ? activeLink : "")}>PORTFOLIO</NavLink></div>
+          <div className="p-2 "><NavLink to="/contact/" className={({ isActive }) => (isActive ? activeLink : "")}>CONTACT</NavLink></div>
         </div>
     </div>
         
