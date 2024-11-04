be543d9cf81c124020bf9dba2c9a033a45ecb5f1
alishafulton <af.ui.design@gmail.com>
2024-11-04 17:02:10 -0600

    c

--- src/Components/PortfolioPage/project1.jsx
+++ src/Components/PortfolioPage/project1.jsx
@@ -6,7 +6,7 @@ import Mockup from "./mock1";
 export default function Project1() {
     
       return (
-<div className="text-md tracking-wide">
+<div className="text-md tracking-wide leading-10 text-gray-400 ">
         <div className="w-64 justify-self-center border border-gray-200">
           <div className="w-52">
             <Mockup/>
