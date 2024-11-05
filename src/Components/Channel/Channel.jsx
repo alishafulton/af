61641ffa74106440401ed20099f9de2db59a9fbc
alishafulton <af.ui.design@gmail.com>
2024-11-04 21:48:53 -0600

    b

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -1,3 +1,8 @@
+import channel1 from '../../assets/channel1.png';
+import channel2 from '../../assets/channel2.png';
+import channel3 from '../../assets/channel3.png';
+import channel4 from '../../assets/channel4.png';
+import channel5 from '../../assets/channel5.png';
 
 
 // i imported all of my sections of my portfolio
@@ -7,7 +12,15 @@
 
 export default function Channel () {
     return (
-<></>
+        <div className="flex-col max-w-4xl justify-items-center ">
+        <img src={ channel1 } alt="" className="p-0" />
+        <img src={ channel2 } alt="" className="p-0" />
+        <img src={ channel3 } alt="" className="p-0" />
+        <img src={ channel4 } alt="" className="p-0" />
+        <img src={ channel5 } alt="" className="p-0" />
+
+
+        </div>
 
     );
 };
---
+++ src/Components/Channel/ChannelPrototype.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+// prototypes from figma were a little tricky
+// thankfully they come with embed codes which work nicely
+// but when its displayed there is only so much you can see
+// so we change the width so more is visible 
+// in different screen aspect ratios
+
+
+export default function ChannelPrototype () {
+    return (
+      <div className="flex justify-center">
+          <a href="" 
+              className=" bg-black hover:bg-green text-white font-mono tracking-tight my-4 mx-4 px-5 py-2">
+              Open in a new window
+          </a>
+      </div>
+    
+);
+
+};
