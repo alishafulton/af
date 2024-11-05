a4c0f221227ac4735a2ceb7b0b6c5f1b599fa394
alishafulton <af.ui.design@gmail.com>
2024-11-05 10:15:20 -0600

    Update project1.jsx

--- src/Components/PortfolioPage/project1.jsx
+++ src/Components/PortfolioPage/project1.jsx
@@ -8,8 +8,8 @@ export default function Project1() {
     
       return (
 <div>
-            <div className="w-42 md:w-56 justify-self-center border border-gray-200 group-hover:bg-white group-hover:opacity-5">
-              <div className="w-30 md:w-42 mx-6 filter group-hover:blur-md">
+            <div className="w-42 md:w-56 justify-self-center border filter blur-none border-gray-200 group-hover:bg-white group-hover:opacity-80 group-hover:blur-md">
+              <div className="w-30 md:w-42 mx-6 filter blur group-hover:blur-md">
                  <Mockup/>
               </div>
             </div>
