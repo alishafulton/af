740347ba594fc69be05462a68608b08015840c0d
alishafulton <af.ui.design@gmail.com>
2024-11-04 20:36:29 -0600

    b

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Blurb from "./Blurb";
 import Resume from "./Resume";
+import Compliments from "./Compliments";
 
 
 
@@ -10,6 +11,7 @@ export default function AboutPage() {
         <div className="flex flex-col justify-items-center w-full ">
             <Blurb />
             <Resume />
+            <Compliments/>
         </div>
       );
     }
