7fca0bf3f673f43c668a8838bac830b4d6004590
alishafulton <af.ui.design@gmail.com>
2024-11-04 19:28:40 -0600

    Update Resume.jsx

--- src/Components/AboutPage/Resume.jsx
+++ src/Components/AboutPage/Resume.jsx
@@ -5,6 +5,6 @@ import React from "react";
 export default function Resume() {
     
       return (
-        <div className="w-20 h-8 bg-black text-white font-mono text-xs justify-center ">RESUME</div>
+        <div className="bg-black text-white font-mono text-xs mx-3 my-1">RESUME</div>
       );
     }
