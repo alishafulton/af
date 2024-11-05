d09f23d9552fb9720a829c3d5591640f698fd6dd
alishafulton <af.ui.design@gmail.com>
2024-11-04 19:38:59 -0600

    Update Resume.jsx

--- src/Components/AboutPage/Resume.jsx
+++ src/Components/AboutPage/Resume.jsx
@@ -5,6 +5,6 @@ import React from "react";
 export default function Resume() {
     
       return (
-        <div className="bg-black text-white font-mono text-xs m-4 px-3 py-1">RESUME</div>
+        <div className="bg-black hover:bg-green text-white w-min font-mono text-xs my-4 px-3 py-1">RESUME</div>
       );
     }
