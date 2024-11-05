78c7eb2c6ab7246dc54f81a39f90b31271ca5387
alishafulton <af.ui.design@gmail.com>
2024-11-04 22:07:16 -0600

    c

--- src/Components/PortfolioPage/PortfolioPage.jsx
+++ src/Components/PortfolioPage/PortfolioPage.jsx
@@ -11,7 +11,7 @@ export default function PortfolioPage() {
     
       return (
         <div className="w-full">
-          <div className="flex flex-row space-x-6 my-6">
+          <div className="flex flex-row space-x-6 md:space-x-10 my-6 md:my-8">
           <NavLink to="/coast/" className={({ isActive }) => (isActive ? activeLink : "")}><Project1 className="m-2" /></NavLink>
           <NavLink to="/channel/" className={({ isActive }) => (isActive ? activeLink : "")}><Project2 className="m-2" /></NavLink>
         </div>
