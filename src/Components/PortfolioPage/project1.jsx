738833a7ca9494162d5e1f63e6f5cbbb1b2919c5
alishafulton <af.ui.design@gmail.com>
2024-11-05 17:05:02 -0600

    c

--- src/Components/PortfolioPage/project1.jsx
+++ src/Components/PortfolioPage/project1.jsx
@@ -8,7 +8,7 @@ export default function Project1() {
     
       return (
 <div>
-            <div className="w-42 md:w-56 justify-self-center border border-gray-200 group-hover:bg-pink group-hover:bg-opacity-80 group-hover:border-pink">
+            <div className="w-42 md:w-56 justify-self-center border border-gray-200 group-hover:bg-pink group-hover:bg-opacity-10 group-hover:border-pink">
               <div className=" w-30 md:w-42 mx-6  transition-all ease-in-out duration-200">
                  <Mockup/>
               </div>
