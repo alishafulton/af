eba9f0de2378c14240bacff8c7ef1c392e31b2ca
alishafulton <af.ui.design@gmail.com>
2024-11-04 20:07:32 -0600

    v

--- src/Components/PortfolioPage/project1.jsx
+++ src/Components/PortfolioPage/project1.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Mockup from "./mock1";
+import UxTag from "./uxtag";
 
 
 
 export default function Project1() {
     
       return (
-<div className="text-lg  leading-8 text-black ">
-        <div className="w-56 justify-self-center border border-gray-200">
-          <div className="w-44 mx-6">
-            <Mockup/>
+<div>
+            <div className="w-56 justify-self-center border border-gray-200">
+              <div className="w-44 mx-6">
+                 <Mockup/>
+              </div>
+            </div>
+        
+            <div className="flex flex-row text-lg leading-8 text-black ">Coast
+          <UxTag/>
           </div>
-          </div>
-          Coast
 </div>
 
       );
---
+++ src/Components/PortfolioPage/uxtag.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function UxTag() {
+    
+    return (
+        <div className="bg-pink bg-opacity-25 text-xs font-sans tracking-tight text-pink px-1 py-0 ">UX Case Study</div>
+    );
+}
