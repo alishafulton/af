3ea0c8e744b54e4f4d5f087f617f1c6120034524
alishafulton <af.ui.design@gmail.com>
2024-11-05 17:52:52 -0600

    Update header.jsx

--- src/Components/Contact/header.jsx
+++ src/Components/Contact/header.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default function Header() {
   return (<div className="w-full justify-items-center my-12">
-    <div className="font-fancy text-6xl tracking-tightest ">say howdy</div></div>
+    <div className="font-fancy text-5xl tracking-tighter ">say howdy</div></div>
   );
 }
