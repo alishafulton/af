565fa5421d8b1d21c7eab96daeeb90ca94603139
alishafulton <af.ui.design@gmail.com>
2024-11-04 19:31:33 -0600

    Update AboutPage.jsx

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -7,7 +7,7 @@ import Resume from "./Resume";
 export default function AboutPage() {
     
       return (
-        <div className="w-full justify-items-center">
+        <div className="justify-items-center">
             <Blurb />
             <Resume />
         </div>
