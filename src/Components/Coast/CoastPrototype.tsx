682d60d24071c49335847eabf2835901a84cce9b
alishafulton <af.ui.design@gmail.com>
2024-11-04 18:46:17 -0600

    Update CoastPrototype.tsx

--- src/Components/Coast/CoastPrototype.tsx
+++ src/Components/Coast/CoastPrototype.tsx
@@ -10,10 +10,11 @@ import React from "react"
 export default function CoastPrototype () {
     return (
       <div className="h-72">
+      <a href="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fn1MAVIcv7nkcqeTpwo0CpA%2F3.3--Primary-%2526-Secondary-Actions%3Fpage-id%3D0%253A44%26node-id%3D1118-714%26viewport%3D684%252C531%252C0.18%26t%3DE4LyJHnr7f3kuQ4t-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D1118%253A696">
       <div className="justify-center h-12 w-1/3 bg-black text-white">
-       <a href="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Fn1MAVIcv7nkcqeTpwo0CpA%2F3.3--Primary-%2526-Secondary-Actions%3Fpage-id%3D0%253A44%26node-id%3D1118-714%26viewport%3D684%252C531%252C0.18%26t%3DE4LyJHnr7f3kuQ4t-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D1118%253A696">
-       Open in a new window</a>
+       Open in a new window
     </div>
+    </a>
     </div>
     
 );
