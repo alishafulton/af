7b171b72bb191ea12da9458f9170884bcd8a96a3
alishafulton <af.ui.design@gmail.com>
2024-11-05 17:31:21 -0600

    q

--- src/Components/Contact/header.jsx
+++ src/Components/Contact/header.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default function header() {
   return (
-    <div className="font-fancy">say howdy</div>
+    <div className="font-fancy text-lg">say howdy</div>
   );
 }
