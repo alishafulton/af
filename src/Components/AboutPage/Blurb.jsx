99c201060e6e503c5961e4581f79ba103e16abb7
alishafulton <af.ui.design@gmail.com>
2024-11-05 09:48:56 -0600

    q

--- src/Components/AboutPage/Blurb.jsx
+++ src/Components/AboutPage/Blurb.jsx
@@ -7,7 +7,7 @@ export default function Blurb() {
     
       return (
         <div className = "flex flex-row space-x-6 h-min">
-            <div className="w-40 "><Selfie/></div>
+            <div className="w-40 md:w-44 "><Selfie/></div>
         
         <div className="w-56 md:w-96 text-2xs md:text-xs font-mono">
         I am a lifelong learner of educational psychology, including human behavior, motivations, and learning processes. I am an empathic and active listener who has had years of observational research of others. I am grateful to constantly grow my technology knowledge online. I consider myself to be self-taught on a wide variety of subjects and have taken computer science classes, ux courses through Google and Coursera, and am always challenging myself to learn more. I would be more than thrilled to discuss how my years of retail experience, my degree in education and my obsession with design in technology are all a part of what makes me an incredible user experience designer. 
