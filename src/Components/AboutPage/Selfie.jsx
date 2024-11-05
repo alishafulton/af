911bc26c7068d711223f742e8cd9cf234eff2e28
alishafulton <af.ui.design@gmail.com>
2024-11-05 13:35:32 -0600

    Update Selfie.jsx

--- src/Components/AboutPage/Selfie.jsx
+++ src/Components/AboutPage/Selfie.jsx
@@ -3,9 +3,10 @@ import selfie from '../../assets/Selfie.png'
 
 
 export default function Selfie() {
-    
+  const activeOver = "text-green ";
+
       return (
-        <div className="filter hover:bg-white hover:opacity-75 blur-none hover:blur backdrop-blur ">
+        <div className=" hover:bg-white hover:opacity-75 blur">
         <img src ={ selfie } alt="" className="h-full w-full "/>
         </div>
       );
