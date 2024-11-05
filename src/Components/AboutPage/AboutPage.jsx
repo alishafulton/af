a9e66046b11bec25c9d06d8e7b7f197344d43758
alishafulton <af.ui.design@gmail.com>
2024-11-04 18:40:25 -0600

    Update AboutPage.jsx

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -5,6 +5,8 @@ import React from "react";
 export default function AboutPage() {
     
       return (
-        <></>
+        <div className="w-60 text-xs font-sans">
+        I am a lifelong learner of education and psychology, understanding of human motivations and desires, and an empathic and active listener who has had years of observational research of others. I am grateful to constantly grow my technology knowledge online. I consider myself to be self-taught on a wide variety of subjects and have taken computer science classes, ux courses through Google and Coursera, and am always challenging myself to learn more. I would be more than thrilled to discuss how my years of retail experience, my degree in education and my obsession with design in technology are all a part of what makes me an incredible user experience designer. 
+        </div>
       );
     }
