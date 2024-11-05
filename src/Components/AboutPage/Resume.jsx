ec738b31b7fcded17851d8c1b3b527dea23df154
alishafulton <af.ui.design@gmail.com>
2024-11-04 20:16:30 -0600

    Update Resume.jsx

--- src/Components/AboutPage/Resume.jsx
+++ src/Components/AboutPage/Resume.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Resume() {
     
       return (
-        <div className="flex w-full justify-items-center">
+        <div className="flex w-full justify-center">
         <div className="bg-black hover:bg-green text-white w-min font-mono text-xs my-4 px-3 py-1">RESUME</div></div>
       );
     }
