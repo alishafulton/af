74f03587700ada8928f70d348ce2d07b8a03b741
alishafulton <af.ui.design@gmail.com>
2024-11-05 12:01:48 -0600

    Update mock1.jsx

--- src/Components/PortfolioPage/mock1.jsx
+++ src/Components/PortfolioPage/mock1.jsx
@@ -5,7 +5,7 @@ export default function Mockup() {
     
     return (
         <>
-            <img src={ mock1 } alt="" className="w-full h-full blur-sm" />
+            <img src={ mock1 } alt="" className="w-full h-full filter blur-sm" />
         </>
     );
 }
