f419f5eb79c7631acba39311f2a05d41da5c3872
alishafulton <af.ui.design@gmail.com>
2024-11-04 20:31:34 -0600

    g

--- src/Components/PortfolioPage/uxtag.jsx
+++ src/Components/PortfolioPage/uxtag.jsx
@@ -4,6 +4,6 @@ import React from 'react';
 export default function UxTag() {
     
     return (
-        <div className="bg-pink bg-opacity-25 text-xs rounded-xl border border-opacity-70 border-pink font-sans tracking-tight text-pink text-opacity-70 px-3 py-1 my-1 mx-2">UX Case Study</div>
+        <div className="bg-pink bg-opacity-25 text-xs rounded-xl border border-opacity-70 border-pink font-sans tracking-tight text-pink text-opacity-70 px-2 py-1 my-1 mx-1">UX Case Study</div>
     );
 }
