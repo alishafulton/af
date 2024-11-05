bcac05cda4cf3091c6159043febe5e988bd10f70
alishafulton <af.ui.design@gmail.com>
2024-11-04 21:58:38 -0600

    Update Sprig.jsx

--- src/Components/Sprig/Sprig.jsx
+++ src/Components/Sprig/Sprig.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import sprig1 from './../../assets/sprig1.png'
+import sprig1 from './../../assets/sprig1.png';
+import sprig2 from './../../assets/sprig2.png';
 
 // i imported all of my sections of my portfolio
 // separately as components 
@@ -8,9 +9,10 @@ import sprig1 from './../../assets/sprig1.png'
 
 export default function Sprig () {
     return (
-<>
+        <div className="flex-col max-w-4xl justify-items-center ">
 <img src = {sprig1} alt="" />
-</>
+<img src = {sprig2} alt="" />
+</div>
     );
 };
 
