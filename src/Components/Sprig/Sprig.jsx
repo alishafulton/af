bcbc1d9cfc52a4a03a59e98faded42de989f2aad
alishafulton <af.ui.design@gmail.com>
2024-11-04 22:04:04 -0600

    g

--- src/Components/Sprig/Sprig.jsx
+++ src/Components/Sprig/Sprig.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import sprig1 from './../../assets/sprig1.png';
 import sprig2 from './../../assets/sprig2.png';
+import SprigPrototype from './SprigPrototype';
 
 // i imported all of my sections of my portfolio
 // separately as components 
@@ -11,6 +12,8 @@ export default function Sprig () {
     return (
         <div className="flex-col max-w-4xl justify-items-center ">
 <img src = {sprig1} alt="" />
+<SprigPrototype/>
+
 <img src = {sprig2} alt="" />
 </div>
     );
--- src/Components/Sprig/SprigPrototype.tsx
+++ src/Components/Sprig/SprigPrototype.tsx
@@ -8,15 +8,14 @@ import React from "react"
 
 
 export default function SprigPrototype () {
-
-    return (
-    <div className="mx-12 h-96">
-      <iframe src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F4Sc1LEgwnnOIf8Z31dkYtD%2FSPRIG%3Fpage-id%3D34%253A2%26node-id%3D230-347%26viewport%3D369%252C25%252C0.26%26t%3DHD5UMDuOwPQIH59t-1%26scaling%3Dscale-down%26content-scaling%3Dfixed%26starting-point-node-id%3D230%253A345" 
-      className="h-full w-1/2 "
-    /></div>
-    
-    
+  return (
+    <div className="flex justify-center">
+        <a href="https://www.figma.com/proto/4Sc1LEgwnnOIf8Z31dkYtD/SPRIG?page-id=34%3A2&node-id=230-347&node-type=canvas&viewport=334%2C-659%2C0.36&t=q7QXchFV2Yon6Rw0-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=230%3A345" 
+            className=" bg-black hover:bg-green text-white font-mono tracking-tight my-4 mx-4 px-5 py-2">
+            Open in a new window
+        </a>
+    </div>
+  
 );
-
 
 };
