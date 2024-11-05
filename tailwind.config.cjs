bfd04bb8bd22af0a8100c1116600943cd1066056
alishafulton <af.ui.design@gmail.com>
2024-11-04 20:40:35 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontSize: {
+        xs: '0.5rem',
+      },
       fontFamily: {
         serif: ["Garamond", "serif"],
         sans: ["SFDisplay", "sans"],
