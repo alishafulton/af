623f6ecbced64d722428738fd3f3b37a6f22171e
alishafulton <af.ui.design@gmail.com>
2024-11-04 20:19:07 -0600

    o

--- src/Components/AboutPage/Resume.jsx
+++ src/Components/AboutPage/Resume.jsx
@@ -6,6 +6,6 @@ export default function Resume() {
     
       return (
         <div className="flex w-full justify-center">
-        <div className="bg-black hover:bg-green text-white w-min font-mono text-xs my-4 px-3 py-1">RESUME</div></div>
+        <div className="bg-black hover:bg-green text-white w-min font-mono text-xs my-8 px-3 py-1">RESUME</div></div>
       );
     }
