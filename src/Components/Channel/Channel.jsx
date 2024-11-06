558a552191c6278fe569aaa2a71430b231b082cc
alishafulton <af.ui.design@gmail.com>
2024-11-06 11:04:23 -0600

    Update Channel.jsx

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -29,7 +29,7 @@ export default function Channel () {
 
 
         <div
-            className="scroll-to-top fixed z-30 bottom-6 right-6 py-2 px-4
+            className="fixed z-30 bottom-6 right-6 py-2 px-4
                        bg-pink text-white rounded-full 
                        transition duration-300 hover:bg-gray-700 
                        hover:text-gray-200">
