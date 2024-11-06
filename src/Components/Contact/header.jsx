efff2a6275aa7ee404935db93b2a8be6d61463c1
alishafulton <af.ui.design@gmail.com>
2024-11-05 21:37:27 -0600

    y

--- src/Components/Contact/header.jsx
+++ src/Components/Contact/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-  <div className="flex m-4 justify-center">
+  <div className="flex m-2 justify-center">
     <div className="font-fancy text-5xl tracking-tightest p-2 justify-self-center">say howdy</div></div>
   );
 }
