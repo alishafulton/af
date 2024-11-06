22fd34de83b6b7f4a1783bb47e1922fe92a2da59
alishafulton <af.ui.design@gmail.com>
2024-11-05 20:52:10 -0600

    o

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -8,9 +8,9 @@ import Header from './header';
 
 export default function ContactPage() {
   return (
-<div className = "flex flex-col w-screen p-5 justify-items-center cursor-crosshair ">
+<div className = "flex flex-col w-screen p-5 justify-center cursor-crosshair ">
   <Header/>
-  <ContactForm className="justify-center" />
+  <ContactForm />
 </div>
   );
 }
