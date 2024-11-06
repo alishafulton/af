b98713d256428cc415bc4f8188a54665dbccc7ba
alishafulton <af.ui.design@gmail.com>
2024-11-06 15:48:59 -0600

    Update ContactPage.jsx

--- src/Components/Contact/ContactPage.jsx
+++ src/Components/Contact/ContactPage.jsx
@@ -10,13 +10,13 @@ import DiscoBall from './../../assets/disco.mov';
 export default function ContactPage() {
   return (
     <>
-<div className = "flex flex-col relative w-screen h-lvh p-5 justify-center cursor-crosshair z-60 ">
+<div className = "flex flex-col relative w-screen h-lvh p-5 justify-center cursor-crosshair bg-transparent z-60 ">
   <Header/>
   <ContactForm />
 </div>
 
 
-  <div className="flex absolute top-0 flex-shrink-0  bg-pink">
+  <div className="flex absolute top-0 h-lvh flex-shrink-0 bg-pink z-50">
     <video className="object-cover"
             muted autoPlay loop playsInline disablePictureInPicture >
     <source src={ DiscoBall }  type="video/webm"></source>
