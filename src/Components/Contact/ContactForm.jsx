f0f12aa1ebbbdb4d780daca589d5b20591028f3d
alishafulton <af.ui.design@gmail.com>
2024-11-05 18:36:30 -0600

    v

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -42,9 +42,9 @@ export default function ContactForm() {
     // and finally a beautiful orange submit button :)
 
     return (
-      <div className="flex flex-col justify-center my-2 w-full m-5 text-xs tracking-wide">
+      <div className="flex flex-col justify-center my-2 w-full p-5 text-xs tracking-wide">
 
-        <form onSubmit={onSubmit} className="w-full justify-center">
+        <form onSubmit={onSubmit} className="w-2/3 justify-center">
 
 
           <div className="m-0.5">
--- src/Components/Contact/header.jsx
+++ src/Components/Contact/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-  <div className="my-12 w-min m-4">
+  <div className="my-12 m-4">
     <div className="font-fancy text-5xl tracking-tightest p-2">say howdy</div></div>
   );
 }
