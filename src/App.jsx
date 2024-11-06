c991470b2ecd12d5e0a4ebb1f0ab379f7b3c33a0
alishafulton <af.ui.design@gmail.com>
2024-11-05 18:09:42 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './index.css'
 export default function App () {
   return (
     <>
-      <div className="flex flex-col mx-2">
+      <div className="flex flex-col mx-4">
        <Navbar/>
        <Outlet/>
        </div>
