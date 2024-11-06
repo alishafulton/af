138267326a51161c6dfc65e973fa8ea1e3f4dafb
alishafulton <af.ui.design@gmail.com>
2024-11-05 22:47:17 -0600

    hj

--- src/Components/AboutPage/Blurb.jsx
+++ src/Components/AboutPage/Blurb.jsx
@@ -7,7 +7,7 @@ export default function Blurb() {
     
       return (
         <div className = "flex flex-col-reverse justify-items-center md:flex-row space-x-0 md:space-x-6 mx-2 h-min ">
-            <div className="w-full md:w-min"><Selfie/></div>
+            <div className="w-full md:w-min justify-center"><Selfie/></div>
         
         <div className="m-4 md:m-0 text-2xs md:text-xs font-mono text-center md:text-left ">
         I am a lifelong learner of educational psychology, including human behavior, motivations, and learning processes. 
--- src/Components/AboutPage/Selfie.jsx
+++ src/Components/AboutPage/Selfie.jsx
@@ -5,7 +5,7 @@ import selfie from '../../assets/Selfie.png'
 export default function Selfie() {
 
       return (
-        <div className="w-40 md:w-52 justify-self-center">
+        <div className="w-40 md:w-52 justify-center">
         <img src ={ selfie } alt="" className="h-full w-full "/>
         </div>
       );
