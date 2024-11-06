1989d82efa415c1c9fc31ef07efacfd29749acd3
alishafulton <af.ui.design@gmail.com>
2024-11-05 22:37:32 -0600

    v

--- src/Components/AboutPage/Blurb.jsx
+++ src/Components/AboutPage/Blurb.jsx
@@ -9,7 +9,7 @@ export default function Blurb() {
         <div className = "flex flex-col-reverse justify-items-center md:flex-row space-x-0 md:space-x-6 mx-2 h-min ">
             <div className="justify-center w-full md:w-min"><Selfie/></div>
         
-        <div className="w-76 md:w-96 text-2xs md:text-xs font-mono text-center md:text-left ">
+        <div className="m-4 md:m-0 text-2xs md:text-xs font-mono text-center md:text-left ">
         I am a lifelong learner of educational psychology, including human behavior, motivations, and learning processes. 
         I am an empathic and active listener who has had years of observational research of others. My 8 years with Starbucks (and licensed stores!) have given me a unique view of the customer-business relationship and how to problem-solve in a moments' notice. 
         I am grateful to constantly grow my technology knowledge online, and I consider myself to be self-taught on a wide variety of subjects. I've taken computer science classes through Colorado State, ux courses through Google and Coursera, and am always learning something new. 
--- src/Components/AboutPage/Selfie.jsx
+++ src/Components/AboutPage/Selfie.jsx
@@ -5,7 +5,7 @@ import selfie from '../../assets/Selfie.png'
 export default function Selfie() {
 
       return (
-        <div className="w-40 md:w-52">
+        <div className="w-40 md:w-52 justify-center">
         <img src ={ selfie } alt="" className="h-full w-full "/>
         </div>
       );
