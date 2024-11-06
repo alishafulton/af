ab1e5d47e619133344b3ac8665beb808e0c20187
alishafulton <af.ui.design@gmail.com>
2024-11-06 16:38:05 -0600

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer () {
 
 return (
       
-      <div className="flex justify-center justify-items-center py-5 bg-white ">
+      <div className="flex justify-center justify-items-center py-5 bg-white border-t border-opacity-10">
         <div className="flex flex-row font-mono text-sm text-center tracking-tighter text-black">|
         </div>
     </div>
