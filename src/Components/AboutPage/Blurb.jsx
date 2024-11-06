5df06411fafd837634651817e590042903842532
alishafulton <af.ui.design@gmail.com>
2024-11-05 22:17:10 -0600

    Update Blurb.jsx

--- src/Components/AboutPage/Blurb.jsx
+++ src/Components/AboutPage/Blurb.jsx
@@ -6,7 +6,7 @@ import Selfie from "./Selfie";
 export default function Blurb() {
     
       return (
-        <div className = "flex flex-col-reverse justify-center md:flex-row space-x-0 md:space-x-6 h-min ">
+        <div className = "grid grid-cols-1 justify-center md:flex-row space-x-0 md:space-x-6 h-min ">
             <div className="justify-self-center"><Selfie/></div>
         
         <div className="w-76 md:w-96 text-2xs md:text-xs font-mono text-center md:text-left ">
