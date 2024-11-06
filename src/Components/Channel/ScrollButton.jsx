ea347bfbeb7bd6be0e0f6dfe7d6f040ef9ed2264
alishafulton <af.ui.design@gmail.com>
2024-11-06 00:01:31 -0600

    d

---
+++ src/Components/Channel/ScrollButton.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const ScrollButton = () => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true);
+        } else if (scrolled <= 300) {
+            setVisible(false);
+        }
+    };
+}
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+            /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+        });
+    };
+
+
+    window.addEventListener("scroll", toggleVisible);
+
+export default function ScrollButton() {
+    return (
+    <button onclick= { scrollToTop }
+            className="scroll-to-top fixed z-30 bottom-6 right-6 py-2 px-4
+                       bg-pink text-white rounded-full 
+                       transition duration-300 hover:bg-gray-700 
+                       hover:text-gray-200">
+                        <div className="w-min" style={{ display: visible ? "inline" : "none"}}>up</div></button> 
+                        
+                    );
+                }
