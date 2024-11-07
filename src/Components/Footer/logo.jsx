a54d0e0f1962b4ff6b60d31f244deac405b5deae
alishafulton <af.ui.design@gmail.com>
2024-11-06 19:04:34 -0600

    Update logo.jsx

--- src/Components/Footer/logo.jsx
+++ src/Components/Footer/logo.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import logo from '../../assets/logo.png';
+import logohover from '../../assets/logohover.png';
 
 export default function Logo() {
 
     return(
-        <><img src={logo} className="h-6 w-6"/></>
+        <>
+        <img  src={ logo }
+ onMouseOver={(e) => (e.currentTarget.src = { logohover })}
+    onMouseLeave={(e) => 
+      (e.currentTarget.src = { logo })}
+
+          />
+        </>
     );
 }
