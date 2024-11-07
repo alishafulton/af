99aa625a919535746a4a16feed8ac62c8ebe026e
alishafulton <af.ui.design@gmail.com>
2024-11-07 10:07:11 -0600

    Update Sprig.jsx

--- src/Components/Sprig/Sprig.jsx
+++ src/Components/Sprig/Sprig.jsx
@@ -9,13 +9,49 @@ import SprigPrototype from './SprigPrototype';
 
 
 export default function Sprig () {
-    return (
-        <div className="flex-col max-w-4xl justify-items-center ">
-<img src = {sprig1} alt="" />
-<SprigPrototype/>
 
-<img src = {sprig2} alt="" />
-</div>
+    const [showBtn, setShowBtn] = useState("myBtn none");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+      scrollFunction();
+    };
+  
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        setShowBtn("myBtn");
+      } else {
+        setShowBtn("none");
+      }
+    }
+  
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+
+    return (
+        <div>
+            <div className="flex-col max-w-4xl justify-items-center ">
+              <img src = {sprig1} alt="" />
+                <SprigPrototype/>
+              <img src = {sprig2} alt="" />
+            </div>
+
+
+        <div>
+        <button
+        onClick={topFunction}
+        id="myBtn"
+        className={showBtn}
+        title="Go to top" >top
+      </button>
+                     </div>
+        </div>
     );
 };
 
