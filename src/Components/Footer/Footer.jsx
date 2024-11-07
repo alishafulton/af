5a048152dfbd755f66794d336eaea725aa03f33f
alishafulton <af.ui.design@gmail.com>
2024-11-06 18:01:01 -0600

    h

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "./logo";
 
 
 // my navbar! 
@@ -9,7 +10,7 @@ export default function Footer () {
 return (
       
       <div className="flex justify-center justify-items-center py-5 bg-white border-black border-t border-opacity-10">
-        <div className="flex flex-row font-mono text-sm text-center tracking-tighter text-black">|
+        <div className="flex flex-row font-mono text-sm text-center tracking-tighter text-black"><Logo/>
         </div>
     </div>
         
---
+++ src/Components/Footer/logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from '../../assets/logo.png';
+
+export default function Logo() {
+
+    return(
+        <><img src={logo} className="h-6 w-6"/></>
+    );
+}
