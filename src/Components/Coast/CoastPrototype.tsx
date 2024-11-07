01c210d196e139d24e7725667094b11ae6468e4a
alishafulton <af.ui.design@gmail.com>
2024-11-07 13:09:50 -0600

    Update CoastPrototype.tsx

--- src/Components/Coast/CoastPrototype.tsx
+++ src/Components/Coast/CoastPrototype.tsx
@@ -17,8 +17,9 @@ export default function CoastPrototype () {
 src="https://embed.figma.com/proto/n1MAVIcv7nkcqeTpwo0CpA/3.3--Primary-%26-Secondary-Actions?page-id=0%3A44&node-id=1118-714&node-type=canvas&viewport=362%2C550%2C0.1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1118%3A696&embed-host=share" 
 className="flex"/>
 
-<br/><a href="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F6qN9WFCdBbpowtNV9vJwQ1%2Fbound%3Fpage-id%3D5%253A2%26node-id%3D453-34027%26starting-point-node-id%3D474%253A35060%26t%3DvMPeVuUw8dw5Z00Z-1">
-Open in a new window</a>
+<br/>
+<a href="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F6qN9WFCdBbpowtNV9vJwQ1%2Fbound%3Fpage-id%3D5%253A2%26node-id%3D453-34027%26starting-point-node-id%3D474%253A35060%26t%3DvMPeVuUw8dw5Z00Z-1">
+<div className=" flex w-min px-2 bg-red-600 ">Open in a new window</div></a>
 
 </div>
 </div>
