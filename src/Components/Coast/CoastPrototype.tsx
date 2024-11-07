b0b8342723285c16d807a16bcd42ddab7487c4f0
alishafulton <af.ui.design@gmail.com>
2024-11-07 13:43:45 -0600

    Update CoastPrototype.tsx

--- src/Components/Coast/CoastPrototype.tsx
+++ src/Components/Coast/CoastPrototype.tsx
@@ -12,8 +12,8 @@ export default function CoastPrototype () {
 
 <div className="flex w-full h-auto justify-items-center justify-center my-4">
 
-<div className="flex flex-col mx-12 w-full h-[30rem] justify-items-center justify-center">
-<iframe 
+<div className="flex flex-col mx-12 w-full justify-items-center justify-center">
+<iframe className=" h-[30rem] "
 src="https://embed.figma.com/proto/n1MAVIcv7nkcqeTpwo0CpA/3.3--Primary-%26-Secondary-Actions?page-id=0%3A44&node-id=1118-714&node-type=canvas&viewport=362%2C550%2C0.1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1118%3A696&embed-host=share" />
 
 <br/>
