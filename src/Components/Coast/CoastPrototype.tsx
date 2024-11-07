eb844df2116e43099393bd79f840eb67d202e575
alishafulton <af.ui.design@gmail.com>
2024-11-07 13:36:33 -0600

    Update CoastPrototype.tsx

--- src/Components/Coast/CoastPrototype.tsx
+++ src/Components/Coast/CoastPrototype.tsx
@@ -12,10 +12,9 @@ export default function CoastPrototype () {
 
 <div className="flex w-full h-auto justify-items-center justify-center my-4">
 
-<div id="prototype">
+<div className="flex flex-col mx-12 w-full h-[30rem] justify-items-center justify-center">
 <iframe 
-src="https://embed.figma.com/proto/n1MAVIcv7nkcqeTpwo0CpA/3.3--Primary-%26-Secondary-Actions?page-id=0%3A44&node-id=1118-714&node-type=canvas&viewport=362%2C550%2C0.1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1118%3A696&embed-host=share" 
-/>
+src="https://embed.figma.com/proto/n1MAVIcv7nkcqeTpwo0CpA/3.3--Primary-%26-Secondary-Actions?page-id=0%3A44&node-id=1118-714&node-type=canvas&viewport=362%2C550%2C0.1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1118%3A696&embed-host=share" />
 
 <br/>
 <a href="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F6qN9WFCdBbpowtNV9vJwQ1%2Fbound%3Fpage-id%3D5%253A2%26node-id%3D453-34027%26starting-point-node-id%3D474%253A35060%26t%3DvMPeVuUw8dw5Z00Z-1">
