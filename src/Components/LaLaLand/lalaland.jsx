e71ec771a60aae916025574538b40f71f20609bc
alishafulton <af.ui.design@gmail.com>
2024-11-07 10:04:41 -0600

    Update lalaland.jsx

--- src/Components/LaLaLand/lalaland.jsx
+++ src/Components/LaLaLand/lalaland.jsx
@@ -4,7 +4,7 @@ import lala1 from '../../assets/2.mp4';
 import lala2 from '../../assets/3.mp4';
 import lala3 from '../../assets/4.mp4';
 import lala4 from '../../assets/5.png';
-
+ 
 
 // i imported all of my sections of my portfolio
 // separately as components 
@@ -12,8 +12,38 @@ import lala4 from '../../assets/5.png';
 
 
 export default function LaLaLand () {
+
+
+    const [showBtn, setShowBtn] = useState("myBtn none");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+      scrollFunction();
+    };
+  
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        setShowBtn("myBtn");
+      } else {
+        setShowBtn("none");
+      }
+    }
+  
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+
+
+
+
+
     return (
-        <div className="justify-center">
+        <div>
             <div className="flex flex-col max-w-4xl justify-center justify-items-center ">
 
                 <div className="flex">
@@ -51,6 +81,15 @@ export default function LaLaLand () {
                 </div>
 
 </div>
+
+<div>
+        <button
+        onClick={topFunction}
+        id="myBtn"
+        className={showBtn}
+        title="Go to top" >top
+      </button>
+                     </div>
 
 </div>
 
