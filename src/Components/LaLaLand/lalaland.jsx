cfa27f89f99c390b04fcf585f68382087269c6f4
alishafulton <af.ui.design@gmail.com>
2024-11-06 19:51:40 -0600

    c

--- src/Components/LaLaLand/lalaland.jsx
+++ src/Components/LaLaLand/lalaland.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import lala1 from '../../assets/1.png';
+import lala2 from '../../assets/2.png';
+import lala3 from '../../assets/3.png';
 
 
 // i imported all of my sections of my portfolio
@@ -9,9 +11,38 @@ import React from 'react';
 
 export default function LaLaLand () {
     return (
-
         <div>
-        </div>
+            <div className="flex flex-col max-w-4xl justify-items-center ">
+
+                <div className="flex bg-pink z-40">
+                    <video className="object-cover "
+                        muted autoPlay loop playsInline disablePictureInPicture >
+                            <source src={ lala1 }  type="video/webm">
+                            </source>
+                    </video>
+                </div>
+
+
+                <div className="flex bg-pink z-40">
+                    <video className="object-cover "
+                        muted autoPlay loop playsInline disablePictureInPicture >
+                            <source src={ lala2 }  type="video/webm">
+                            </source>
+                    </video>
+                </div>
+
+
+                <div className="flex bg-pink z-40">
+                    <video className="object-cover "
+                        muted autoPlay loop playsInline disablePictureInPicture >
+                            <source src={ lala3 }  type="video/webm">
+                            </source>
+                    </video>
+                </div>
+
+</div>
+
+</div>
 
     );
 };
