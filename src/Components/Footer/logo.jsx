30046b267a6d19eb692f761ff2b01211691cd100
alishafulton <af.ui.design@gmail.com>
2024-11-06 19:28:18 -0600

    Update logo.jsx

--- src/Components/Footer/logo.jsx
+++ src/Components/Footer/logo.jsx
@@ -3,14 +3,20 @@ import logo from '../../assets/logo.png';
 import logohover from '../../assets/logohover.png';
 
 export default function Logo() {
+    const [isHovering, setIsHovered] = useState(false);
+    const onMouseEnter = () => setIsHovered(true);
+    const onMouseLeave = () => setIsHovered(false);
+    return (
+<div className="flex items-center flex-shrink-0 mr-6 cursor-pointer"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}>
 
-    return(
-        <>
-        <img src={ logo }
-            onMouseOver={(e) => (e.currentTarget.src = { logohover })}
-            onMouseLeave={(e) =>  (e.currentTarget.src = { logo })}
-            className="w-6 h-6"
-          />
-        </>
-    );
-}
+            <div>
+              {isHovering ? (
+                <img src={logo} className="w-6 h-6" />
+              ) : (
+                <img src={logohover} className="w-6 h-6" />
+              )}
+            </div></div>
+        );
+      };
